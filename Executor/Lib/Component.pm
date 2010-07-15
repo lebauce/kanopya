3fe1e4d513360764f9cafe332ed68b667bd97be6
Antoine Castaing <antoine.castaing@hederatech.com>
2010-07-15 12:24:19 +0200

    Creation of Motherboard, Operation and Component class
    Motherboard may be deleted because this object may not have some execution method
    Operation and component class are abstract class.

---
+++ Executor/Lib/Component.pm
@@ -0,0 +1,88 @@
+# Component.pm - 
+
+# Copyright (C) 2009, 2010, 2011, 2012, 2013
+#   Free Software Foundation, Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+# Maintained by Dev Team of Hedera Technology <dev@hederatech.com>.
+# Created 14 july 2010
+
+=head1 NAME
+
+Component - Abstract class of component object
+
+=head1 SYNOPSIS
+
+
+
+=head1 DESCRIPTION
+
+Component is an abstract class of component objects
+
+=head1 METHODS
+
+=cut
+package Component;
+
+use strict;
+use warnings;
+use Log::Log4perl "get_logger";
+use vars qw(@ISA $VERSION);
+
+my $log = get_logger("executor");
+
+$VERSION = do { my @r = (q$Revision: 0.1 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+
+=head2 new
+
+    my comp = Component->new();
+
+Component::new creates a new component object.
+
+=cut
+
+sub new {
+    my $class = shift;
+    my $self = {};
+
+	$log->warn("New Object Component");    
+    bless $self, $class;
+        
+    $self->_init();
+    
+    return $self;
+}
+
+=head2 _init
+
+Executor::_init is a private method used to define internal parameters.
+
+=cut
+
+sub _init {
+	my $self = shift;
+
+	return;
+}
+
+
+=head1 AUTHOR
+
+Copyright (c) 2010 by Hedera Technology Dev Team (dev@hederatech.com). All rights reserved.
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
---
+++ Executor/Lib/Operation.pm
@@ -0,0 +1,88 @@
+# Operation.pm - 
+
+# Copyright (C) 2009, 2010, 2011, 2012, 2013
+#   Free Software Foundation, Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+# Maintained by Dev Team of Hedera Technology <dev@hederatech.com>.
+# Created 14 july 2010
+
+=head1 NAME
+
+Operation - Abstract class of operation object
+
+=head1 SYNOPSIS
+
+
+
+=head1 DESCRIPTION
+
+Component is an abstract class of operation objects
+
+=head1 METHODS
+
+=cut
+package Operation;
+
+use strict;
+use warnings;
+use Log::Log4perl "get_logger";
+use vars qw(@ISA $VERSION);
+
+my $log = get_logger("executor");
+
+$VERSION = do { my @r = (q$Revision: 0.1 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+
+=head2 new
+
+    my comp = Operation->new();
+
+Operation->new creates a new operation object.
+
+=cut
+
+sub new {
+    my $class = shift;
+    my $self = {};
+
+	$log->warn("New Object Operation");    
+    bless $self, $class;
+        
+    $self->_init();
+    
+    return $self;
+}
+
+=head2 _init
+
+Executor::_init is a private method used to define internal parameters.
+
+=cut
+
+sub _init {
+	my $self = shift;
+
+	return;
+}
+
+
+=head1 AUTHOR
+
+Copyright (c) 2010 by Hedera Technology Dev Team (dev@hederatech.com). All rights reserved.
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
