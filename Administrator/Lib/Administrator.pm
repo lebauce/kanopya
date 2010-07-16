0eb67a356706dbf6098cfd01904473d439404bcf
Antoine Castaing <antoine.castaing@hederatech.com>
2010-07-16 03:21:25 +0200

    New Executor

--- Administrator/Lib/Administrator.pm
+++ Administrator/Lib/Administrator.pm
@@ -1,3 +1,49 @@
+# Administrator.pm - Object class of Administrator server
+
+# Copyright (C) 2009, 2010, 2011, 2012, 2013
+#   Free Software Foundation, Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+# Maintained by Dev Team of Hedera Technology <dev@hederatech.com>.
+# Created 14 july 2010
+
+=head1 NAME
+
+Administrator - Administrator object
+
+=head1 SYNOPSIS
+
+    use Executor;
+    
+    # Creates executor
+    my $adm = Administrator->new();
+    
+    # Get object
+    $adm->getobject($type : String, %ObjectDefinition);
+
+
+=head1 DESCRIPTION
+
+Administrator is the main object use to create administrator objects
+
+=head1 METHODS
+
+=cut
+
 package Administrator;
 
 
@@ -97,4 +143,23 @@ sub newObj {}
 
 sub saveObj {}
 
+=head2 getNextOperation
+
+	adm->getNextOperation() : Operationdata send the next operation.
+
+=cut
+
+sub getNextOperation {
+	
+}
+
 1;
+
+__END__
+
+=head1 AUTHOR
+
+Copyright (c) 2010 by Hedera Technology Dev Team (dev@hederatech.com). All rights reserved.
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
