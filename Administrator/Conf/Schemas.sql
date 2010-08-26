8db7e19b096dff24ba3ad8357433467a917f11b1
Antoine Castaing <antoine.castaing@hederatech.com>
2010-08-26 17:31:43 +0200

    Add Foregin key

--- Administrator/Conf/Schemas.sql
+++ Administrator/Conf/Schemas.sql
@@ -433,7 +433,7 @@ CREATE TABLE `iscsitarget1_target` (
   `mountpoint` char(64) DEFAULT NULL,
   `mount_option` char(32) DEFAULT NULL,
   PRIMARY KEY (`iscsitarget1_target_id`),
-  UNIQUE KEY `iscsitarget1_UNIQUE` (`iscsitarget1_target_name`)
+  UNIQUE KEY `iscsitarget1_UNIQUE` (`iscsitarget1_target_name`),
   UNIQUE KEY `fk_iscsitarget1_1` (`component_instance_id`),
   CONSTRAINT `fk_iscsitarget1_1` FOREIGN KEY (`component_instance_id`) REFERENCES `component_instance` (`component_instance_id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
