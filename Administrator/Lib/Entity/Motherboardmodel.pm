b3df9751a969e1e733d52cdfc3337fa7bd31b94a
Thomas MANNI <thomas.manni@hederatech.com>
2010-09-07 16:46:11 +0200

    rewrite of Motherboardmodel / Processormodel entities

---
+++ Administrator/Lib/Entity/Motherboardmodel.pm
@@ -0,0 +1,137 @@
+package Entity::Motherboardmodel;
+
+use strict;
+use lib qw(/workspace/mcs/Administrator/Lib /workspace/mcs/Common/Lib);
+use McsExceptions;
+use base "Entity";
+
+use Log::Log4perl "get_logger";
+my $log = get_logger("administrator");
+my $errmsg;
+
+use constant ATTR_DEF => {
+	motherboardmodel_brand => { pattern => 'm//s', is_mandatory => 1, is_extended => 0 },
+	motherboardmodel_name => { pattern => 'm//s', is_mandatory => 1, is_extended => 0 },
+	motherboardmodel_chipset => { pattern => 'm//s', is_mandatory => 0, is_extended => 0 },
+	motherboardmodel_processor_num => { pattern => 'm//s', is_mandatory => 0, is_extended => 0 },
+	motherboardmodel_consumption => { pattern => 'm//s', is_mandatory => 1, is_extended => 0 },
+	motherboardmodel_iface_num => { pattern => 'm//s', is_mandatory => 0, is_extended => 0 },
+	motherboardmodel_ram_slot_num => { pattern => 'm//s', is_mandatory => 0, is_extended => 0 },
+	motherboardmodel_ram_max => { pattern => 'm//s', is_mandatory => 0, is_extended => 0 },
+	processormodel_id => { pattern => 'm//s', is_mandatory => 0, is_extended => 0 },
+};
+
+
+
+=head2 checkAttrs
+	
+	Desc : This function check if new object data are correct and sort attrs between extended and global
+	args: 
+		class : String : Real class to check
+		data : hashref : Entity data to be checked
+	return : hashref of hashref : a hashref containing 2 hashref, global attrs and extended ones
+
+=cut
+
+sub checkAttrs {
+	# Remove class
+	shift;
+	my %args = @_;
+	my (%global_attrs, %ext_attrs, $attr);
+	my $attr_def = ATTR_DEF;
+	#print Dumper $attr_def;
+	if (! exists $args{attrs} or ! defined $args{attrs}){ 
+		$errmsg = "Entity::Motherboardmodel->checkAttrs need an attrs hash named argument!";
+		$log->error($errmsg);
+		throw Mcs::Exception::Internal::IncorrectParam(error => $errmsg);
+	}	
+
+	my $attrs = $args{attrs};
+	foreach $attr (keys(%$attrs)) {
+		if (exists $attr_def->{$attr}){
+			$log->debug("Field <$attr> and value in attrs <$attrs->{$attr}>");
+			#TODO Check param with regexp in pattern field of struct
+			if ($attr_def->{$attr}->{is_extended}){
+				$ext_attrs{$attr} = $attrs->{$attr};
+			}
+			else {
+				$global_attrs{$attr} = $attrs->{$attr};
+			}
+		}
+		else {
+			$errmsg = "Entity::Motherboardmodel->checkAttrs detect a wrong attr $attr !";
+			$log->error($errmsg);
+			throw Mcs::Exception::Internal::IncorrectParam(error => $errmsg);
+		}
+	}
+	foreach $attr (keys(%$attr_def)) {
+		if (($attr_def->{$attr}->{is_mandatory}) &&
+			(! exists $attrs->{$attr})) {
+				$errmsg = "Entity::Motherboardmodel->checkAttrs detect a missing attribute $attr !";
+				$log->error($errmsg);
+				throw Mcs::Exception::Internal::IncorrectParam(error => $errmsg);
+			}
+	}
+	#TODO Check if id (systemimage, kernel, ...) exist and are correct.
+	return {global => \%global_attrs, extended => \%ext_attrs};
+}
+
+=head2 checkAttr
+	
+	Desc : This function check new object attribute
+	args: 
+		name : String : Attribute name
+		value : String : Attribute value
+	return : No return value only throw exception if error
+
+=cut
+
+sub checkAttr{
+	my $self = shift;
+	my %args = @_;
+	my $attr_def = ATTR_DEF;
+
+	if ((! exists $args{name} or ! defined $args{name}) ||
+		(! exists $args{value})) { 
+		$errmsg = "Entity::Motherboardmodel->checkAttr need a name and value named argument!";
+		$log->error($errmsg);
+		throw Mcs::Exception::Internal::IncorrectParam(error => $errmsg);
+	}
+	if (! defined $args{value} && $attr_def->{$args{name}}->{is_mandatory}){
+		$errmsg = "Entity::Motherboardmodel->checkAttr detect a null value for a mandatory attr ($args{name})";
+		$log->error($errmsg);
+		throw Mcs::Exception::Internal::WrongValue(error => $errmsg);
+	}
+
+	if (!exists $attr_def->{$args{name}}){
+		$errmsg = "Entity::Motherboardmodel->checkAttr invalid attr name : '$args{name}'";
+		$log->error($errmsg);	
+		throw Mcs::Exception::Internal::IncorrectParam(error => $errmsg);
+	}
+
+	# Here check attr value
+}
+
+sub extension { return undef; }
+
+sub new {
+    my $class = shift;
+    my %args = @_;
+
+    if ((! exists $args{data} or ! defined $args{data}) ||
+		(! exists $args{rightschecker} or ! defined $args{rightschecker})) { 
+		$errmsg = "Entity::Motherboardmodel->new need a data and rightschecker named argument!";	
+		$log->error($errmsg);
+		throw Mcs::Exception::Internal::IncorrectParam(error => $errmsg);
+	}
+
+	my $ext_attrs = $args{ext_attrs};
+	delete $args{ext_attrs};
+    my $self = $class->SUPER::new( %args );
+	$self->{_ext_attrs} = $ext_attrs;
+	$self->{extension} = $self->extension();
+    return $self;
+}
+
+
+1;
