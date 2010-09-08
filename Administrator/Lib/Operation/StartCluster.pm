77e88ed39a5a5df2cbbf0a4a763e8ace66f16315
Thomas MANNI <thomas.manni@hederatech.com>
2010-09-08 14:30:02 +0200

    core: StartCluster and StopCluster Operations added

---
+++ Administrator/Lib/Operation/StartCluster.pm
@@ -0,0 +1,123 @@
+# AddCluster.pm - Operation class implementing Cluster start operation
+
+# Copyright (C) 2009, 2010, 2011, 2012, 2013
+#   Free Software Foundation, Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+# Maintained by Dev Team of Hedera Technology <dev@hederatech.com>.
+# Created 14 july 2010
+
+=head1 NAME
+
+Operation::StartCluster - Operation class implementing Cluster start operation
+
+=head1 SYNOPSIS
+
+This Object represent an operation.
+It allows to implement Cluster start operation
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=cut
+package Operation::StartCluster;
+
+use strict;
+use warnings;
+use Log::Log4perl "get_logger";
+use vars qw(@ISA $VERSION);
+use lib qw(/workspace/mcs/Administrator/Lib /workspace/mcs/Common/Lib);
+use base "Operation";
+
+my $log = get_logger("administrator");
+my $errmsg;
+
+$VERSION = do { my @r = (q$Revision: 0.1 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+
+=head2 new
+
+    my $op = Operation::StartCluster->new();
+
+Operation::StartCluster->new creates a new StartCluster operation.
+
+=cut
+
+sub new {
+    my $class = shift;
+    my %args = @_;
+	
+	# presence of 'params' named argument is done in parent class 
+    my $self = $class->SUPER::new( %args );
+    my $admin = $args{administrator};
+     
+	# check if cluster exist in db
+    $log->debug("checking cluster existence with id <$args{params}->{cluster_id}>");
+    my $cluster = $admin->getEntity(type => 'Cluster', id => $args{params}->{cluster_id});
+    
+    # check if cluster is active and down
+    if($cluster->getAttr(name => 'active') == 0 or $cluster->getAttr(name => 'cluster_state') ne 'down') {
+    	my $errmsg = "Operation::StartCluster->new : cluster must be active and down to be started";
+    	$log->error($errmsg);
+    	throw Mcs::Exception::Internal(error => $errmsg);
+    }
+    
+    # check if systemimage is active
+    my $systemimage = $admin->getEntity(type => 'Systemimage', id => $cluster->getAttr(name => 'systemimage_id'));
+    if($systemimage->getAttr(name => 'active') == 0) {
+    	my $errmsg = "Operation::StartCluster->new : cluster's systemimage is not active ; activate it to start the cluster";
+    	$log->error($errmsg);
+    	throw Mcs::Exception::Internal(error => $errmsg);
+    }
+    
+    # check if there are enough free motherboard to start cluster min nodes
+    my @freemotherboards = $admin->getEntities(
+    	type => 'Motherboard', 
+    	hash => { active => 1, motherboard_state => 'down'});
+
+	if(scalar(@freemotherboards) < $cluster->getAttr(name => 'cluster_min_node')) {
+		my $errmsg = "Operation::StartCluster->new : not enough free motherboards to start this cluster ;";
+		$errmsg .= "minimum nodes required is ". $cluster->getAttr(name => 'cluster_min_node');
+		$errmsg .= " and only ".scalar(@freemotherboards). " motherboards are available";
+    	$log->error($errmsg);
+    	throw Mcs::Exception::Internal(error => $errmsg);
+	}    
+
+	return $self;
+}
+
+
+
+=head2 prepare
+
+	$op->prepare();
+
+=cut
+
+sub prepare {
+	my $self = shift;
+	my $adm = Administrator->new();
+}
+
+__END__
+
+=head1 AUTHOR
+
+Copyright (c) 2010 by Hedera Technology Dev Team (dev@hederatech.com). All rights reserved.
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
---
+++ Executor/Lib/EOperation/EStartCluster.pm
@@ -0,0 +1,150 @@
+# EStartCluster.pm - Operation class implementing System image creation operation
+
+# Copyright (C) 2009, 2010, 2011, 2012, 2013
+#   Free Software Foundation, Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+# Maintained by Dev Team of Hedera Technology <dev@hederatech.com>.
+# Created 14 july 2010
+
+=head1 NAME
+
+EEntity::EOperation::EStartCluster - Operation class implementing cluster starting operation
+
+=head1 SYNOPSIS
+
+This Object represent an operation.
+It allows to implement cluster starting operation
+
+=head1 DESCRIPTION
+
+
+
+=head1 METHODS
+
+=cut
+package EOperation::EStartCluster;
+
+use strict;
+use warnings;
+use Log::Log4perl "get_logger";
+use vars qw(@ISA $VERSION);
+use base "EOperation";
+use lib qw(/workspace/mcs/Executor/Lib /workspace/mcs/Common/Lib);
+use McsExceptions;
+use Date::Simple 'time';
+
+
+my $log = get_logger("executor");
+my $errmsg;
+
+$VERSION = do { my @r = (q$Revision: 0.1 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+
+=head2 new
+
+    my $op = EOperation::EStartCluster->new();
+
+EOperation::EStartCluster->new creates a new EStartCluster operation.
+
+=cut
+
+sub new {
+    my $class = shift;
+    my %args = @_;
+    
+    my $self = $class->SUPER::new(%args);
+    $self->_init();
+    
+    return $self;
+}
+
+=head2 _init
+
+	$op->_init() is a private method used to define internal parameters.
+
+=cut
+
+sub _init {
+	my $self = shift;
+
+	return;
+}
+
+=head2 prepare
+
+	$op->prepare();
+
+=cut
+
+sub prepare {
+	my $self = shift;
+	my %args = @_;
+	$self->SUPER::prepare();
+
+	if (! exists $args{internal_cluster} or ! defined $args{internal_cluster}) { 
+		$errmsg = "EStartCluster->prepare need an internal_cluster named argument!";
+		$log->error($errmsg);
+		throw Mcs::Exception::Internal::IncorrectParam(error => $errmsg);
+	}
+	
+	my $adm = Administrator->new();
+	my $params = $self->_getOperation()->getParams();
+
+	$self->{_objs} = {};
+	
+	# Get cluster to start from param
+	$self->{_objs}->{cluster} = $adm->getEntity(type => 'Cluster', id => $params->{cluster_id});
+}
+
+sub execute {
+	my $self = shift;
+	$self->SUPER::execute();
+	my $adm = Administrator->new();
+		
+	$log->info('getting minimum number of nodes to start');
+	my $nodes_to_start = $self->{_objs}->{cluster}->getAttr(name => 'cluster_min_node');	
+	$log->info('getting free motherboards');
+	my @free_motherboards = $adm->getEntities(type => 'Motherboard', hash => { active => 1, motherboard_state => 'down'});
+	
+	my $priority = $self->_getOperation()->getAttr(name => 'priority');
+	
+
+	for(my $i=0 ; $i < $nodes_to_start ; $i++) {
+		my $motherboard = pop @free_motherboards;
+		$adm->newOp(type => 'AddMotherboardInCluster',
+					priority => $priority,
+					params => {
+						cluster_id => $self->{_objs}->{cluster}->getAttr(name => "cluster_id"),
+						motherboard_id => $motherboard->getAttr(name => 'motherboard_id')
+					}
+		);
+	} 	
+	
+	$self->{_objs}->{cluster}->setAttr(name => 'cluster_state', value => 'starting:'.time);
+	$self->{_objs}->{cluster}->save();
+}
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Copyright (c) 2010 by Hedera Technology Dev Team (dev@hederatech.com). All rights reserved.
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
