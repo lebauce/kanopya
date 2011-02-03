631f2418e50015b6f5a2e06da1ceede94b271f2b
tortue <sylvain@hederatech.com>
2011-02-03 13:41:15 +0100

    add schema files

---
+++ lib/administrator/AdministratorDB/Schema/Powersupplycardmodel.pm
@@ -0,0 +1,46 @@
+package AdministratorDB::Schema::Powersupplycardmodel;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components("+AdministratorDB::EntityBase", "Core");
+__PACKAGE__->table("powersupplycardmodel");
+__PACKAGE__->add_columns(
+  "powersupplycardmodel_id",
+  { data_type => "INT", default_value => undef, is_nullable => 0, size => 8 },
+  "powersupplycardmodel_brand",
+  { data_type => "CHAR", default_value => undef, is_nullable => 0, size => 64 },
+  "powersupplycardmodel_name",
+  { data_type => "CHAR", default_value => undef, is_nullable => 0, size => 32 },
+  "powersupplycardmodel_slotscount",
+  { data_type => "INT", default_value => undef, is_nullable => 0, size => 2 },
+);
+__PACKAGE__->set_primary_key("powersupplycardmodel_id");
+__PACKAGE__->add_unique_constraint(
+  "powersupplycardmodel_name_UNIQUE",
+  ["powersupplycardmodel_name"],
+);
+__PACKAGE__->has_many(
+  "powersupplycards",
+  "AdministratorDB::Schema::Powersupplycard",
+  {
+    "foreign.powersupplycardmodel_id" => "self.powersupplycardmodel_id",
+  },
+);
+__PACKAGE__->has_many(
+  "powersupplycardmodel_entities",
+  "AdministratorDB::Schema::PowersupplycardmodelEntity",
+  {
+    "foreign.powersupplycardmodel_id" => "self.powersupplycardmodel_id",
+  },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.04006 @ 2011-02-03 13:34:44
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:GklJEtFHKn6uY9nzpHBjmg
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
---
+++ lib/administrator/AdministratorDB/Schema/PowersupplycardmodelEntity.pm
@@ -0,0 +1,39 @@
+package AdministratorDB::Schema::PowersupplycardmodelEntity;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components("+AdministratorDB::EntityBase", "Core");
+__PACKAGE__->table("powersupplycardmodel_entity");
+__PACKAGE__->add_columns(
+  "entity_id",
+  { data_type => "INT", default_value => undef, is_nullable => 0, size => 8 },
+  "powersupplycardmodel_id",
+  { data_type => "INT", default_value => undef, is_nullable => 0, size => 8 },
+);
+__PACKAGE__->set_primary_key("entity_id", "powersupplycardmodel_id");
+__PACKAGE__->add_unique_constraint("fk_powersupplycardmodel_entity_1", ["entity_id"]);
+__PACKAGE__->add_unique_constraint(
+  "fk_powersupplycardmodel_entity_2",
+  ["powersupplycardmodel_id"],
+);
+__PACKAGE__->belongs_to(
+  "entity_id",
+  "AdministratorDB::Schema::Entity",
+  { entity_id => "entity_id" },
+);
+__PACKAGE__->belongs_to(
+  "powersupplycardmodel_id",
+  "AdministratorDB::Schema::Powersupplycardmodel",
+  { "powersupplycardmodel_id" => "powersupplycardmodel_id" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.04006 @ 2011-02-03 13:34:44
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:uogM/PwGcGTQ6UII7BXpLw
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
