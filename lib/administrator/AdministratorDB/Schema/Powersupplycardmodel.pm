678f6700309efb7c2d34943cb3a776d6ca27ef62
Thomas MANNI <thomas.manni@hederatech.org>
2011-02-08 13:28:48 +0100

    dbix class: entitylink relation added to Powersupplycardmodel.pm

--- lib/administrator/AdministratorDB/Schema/Powersupplycardmodel.pm
+++ lib/administrator/AdministratorDB/Schema/Powersupplycardmodel.pm
@@ -41,6 +41,11 @@ __PACKAGE__->has_many(
 # Created by DBIx::Class::Schema::Loader v0.04006 @ 2011-02-08 12:50:02
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:BtQT+aOdyWhzab3TuD+Psg
 
+__PACKAGE__->has_one(
+  "entitylink",
+  "AdministratorDB::Schema::PowersupplycardmodelEntity",
+  { "foreign.powersupplycardmodel_id" => "self.powersupplycardmodel_id" },
+);
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
