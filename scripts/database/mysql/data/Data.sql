99992d94ea43142fe0275bd2e56f96fbae35b67f
Antoine Castaing <antoine.castaing@hederatech.com>
2011-02-25 17:46:27 +0100

    [Core] Add new operation install component in system image.

--- scripts/database/mysql/data/Data.sql
+++ scripts/database/mysql/data/Data.sql
@@ -120,7 +120,10 @@ INSERT INTO `operationtype` VALUES
 (26,'StartNode'),
 (27,'PreStopNode'),
 (28,'PostStopNode'),
-(29,'PostStartNode');
+(29,'PostStartNode'),
+(30,'InstallComponentInSystemImage'),
+(31,'CreateSharedDisk');
+
 
 INSERT INTO `entity` VALUES (@eid); INSERT INTO `operationtype_entity` VALUES (@eid,1); SET @eid := @eid +1;
 INSERT INTO `entity` VALUES (@eid); INSERT INTO `operationtype_entity` VALUES (@eid,2); SET @eid := @eid +1;
