d6c65c8d8779d734e3cca4317c14c1768b888ee6
Maxime Demoulin <maxime@hederatech.com>
2011-04-01 15:19:08 +0200

    update of meta script for building packages

--- tools/deployment/packaging/builder/build_all.sh
+++ tools/deployment/packaging/builder/build_all.sh
@@ -7,12 +7,12 @@
 #this script aims to build all the kanopya's package at the same time, and has to be launch in the same dir than the other packages building scripts.
 
 
-./package_builder.pl libkanopya-perl $1
-./package_builder.pl kanopya-bootmanager $1
-./package_builder.pl kanopya-core $1
-./package_builder.pl kanopya-storage $1
-./package_builder.pl kanopya-webui $1
-./package_builder.pl kanopya-monitor $1
-./package_builder.pl kanopya-orchestrator $1
-./package_builder.pl kanopya-dev $1
-./package_builder.pl kanopya $1
+./package_builder.pl libkanopya-perl
+./package_builder.pl kanopya-bootmanager
+./package_builder.pl kanopya-core
+./package_builder.pl kanopya-storage
+./package_builder.pl kanopya-webui
+./package_builder.pl kanopya-monitor
+./package_builder.pl kanopya-orchestrator
+./package_builder.pl kanopya-dev
+./package_builder.pl kanopya 
