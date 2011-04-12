ed6e6bfc59633d7f3809d7b3740bd5d30d5beb08
tortue <sylvain@hederatech.com>
2011-04-12 11:57:54 +0200

    No more warning when generate Data.sql (resetdb) and git ignore generated file

--- scripts/database/mysql/sbin/resetdb.sh
+++ scripts/database/mysql/sbin/resetdb.sh
@@ -19,12 +19,4 @@ echo 'done'
 echo -n 'insert initial data... '
 mysql -u $dbuser -p$dbpassword < /opt/kanopya/scripts/database/mysql/data/Data.sql
 echo 'done'
-echo '> WARNING ! <'
-echo 'LVM logical volumes for default distribution and systemimage must be present to make perl tests'
-echo 'You can create them with:'
-echo
-echo '# lvcreate -L 52M -n etc_Debian_5.0 vg1'
-echo '# lvcreate -L 6G -n root_Debian_5.0 vg1'
-echo '# lvcreate -L 52M -n etc_DebianSystemImage vg1'
-echo '# lvcreate -L 6G -n root_DebianSystemImage vg1'
-echo
+
