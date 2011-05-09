3929a55b4106efc83773de4e0f1d857d6de95e9b
tortue <sylvain@hederatech.com>
2011-05-10 01:58:02 +0200

    Add TODO

--- lib/executor/EOperation/EDeployComponent.pm
+++ lib/executor/EOperation/EDeployComponent.pm
@@ -225,6 +225,10 @@ sub execute{
     # Register
     $self->_registerComponentInDB( dbname => $dbname, dbuser => $dbuser, dbpwd => $dbpwd, dbhost => $dbhost, dbport => $dbport );
 
+#   TODO call component data sql (for specific data insertion)
+#   create new component and call init method on it
+
+
     # update distribution provided components list
     my @distributions = Entity::Distribution->getDistributions(hash => {});
     foreach my $distrib (@distributions) {
@@ -260,10 +264,6 @@ sub _registerComponentInDB {
     # Insert template
     $sql_cmd .= "SET \@eid_new_component_template = (SELECT MAX(component_template_id) FROM component_template) + 1;";
     $sql_cmd .= "INSERT INTO component_template VALUES (\@eid_new_component_template,'default_$comp_fullname_lc','/templates/components/$comp_fullname_lc', \@eid_new_component);";
-    
-    # provide component on default distribution (1)
-    $sql_cmd .= "INSERT INTO component_provided VALUES (\@eid_new_component,1);";
-    $sql_cmd .=  "SET foreign_key_checks=1;";
     
     # Execute sql cmd
     my $cmd = "mysql -u $args{dbuser} -p$args{dbpwd} -h $args{dbhost} -P $args{dbport} $args{dbname} -e \"$sql_cmd\"";
