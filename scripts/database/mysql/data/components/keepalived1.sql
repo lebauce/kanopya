739ecbac8f50c50b97cf723de4e8bf7b5a58e178
Antoine Castaing <antoine.castaing@hederatech.com>
2011-05-09 17:54:15 +0200

    [Core] Seperate eache component sql

--- scripts/database/mysql/data/components/keepalived1.sql
+++ scripts/database/mysql/data/components/keepalived1.sql
@@ -3,10 +3,10 @@ SET foreign_key_checks=0;
 
 # Component
 SET @eid_new_component = (SELECT MAX(component_id) FROM component) + 1;
-INSERT INTO `component` VALUES (@eid_new_component,'Php','5','Tool');
+INSERT INTO `component` VALUES (@eid_new_component,'Keepalived','1','Loadbalancer');
 
 # Template
 SET @eid_new_component_template = (SELECT MAX(component_template_id) FROM component_template) + 1;
-INSERT INTO `component_template` VALUES (@eid_new_component_template,'php','/templates/components/php5', @eid_new_component);
+INSERT INTO `component_template` VALUES (@eid_new_component_template,'keepalived','/templates/components/keepalived', @eid_new_component);
 
 SET foreign_key_checks=1;
--- scripts/database/mysql/data/components/syslogng3.sql
+++ scripts/database/mysql/data/components/syslogng3.sql
@@ -3,10 +3,10 @@ SET foreign_key_checks=0;
 
 # Component
 SET @eid_new_component = (SELECT MAX(component_id) FROM component) + 1;
-INSERT INTO `component` VALUES (@eid_new_component,'Php','5','Tool');
+INSERT INTO `component` VALUES (@eid_new_component,'Syslogng','3','Logger');
 
 # Template
 SET @eid_new_component_template = (SELECT MAX(component_template_id) FROM component_template) + 1;
-INSERT INTO `component_template` VALUES (@eid_new_component_template,'php','/templates/components/php5', @eid_new_component);
+INSERT INTO `component_template` VALUES (@eid_new_component_template,'syslogng','/templates/components/syslogng', @eid_new_component);
 
 SET foreign_key_checks=1;
