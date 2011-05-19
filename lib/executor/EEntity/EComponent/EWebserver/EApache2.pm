27eb145e214b6c0775ed95cc468f2d3fac1a450a
Thomas MANNI <thomas.manni@hederatech.com>
2011-05-19 16:01:07 +0200

    [component apache2] mod status configured to allow access from all

--- lib/executor/EEntity/EComponent/EWebserver/EApache2.pm
+++ lib/executor/EEntity/EComponent/EWebserver/EApache2.pm
@@ -19,6 +19,7 @@ use String::Random;
 use Data::Dumper;
 use base "EEntity::EComponent::EWebserver";
 use Log::Log4perl "get_logger";
+use General;
 
 my $log = get_logger("executor");
 my $errmsg;
@@ -28,82 +29,53 @@ my $errmsg;
 sub new {
     my $class = shift;
     my %args = @_;
-
     my $self = $class->SUPER::new( %args );
     return $self;
 }
-
 
 sub addNode {
     my $self = shift;
     my %args = @_;
     
-    if((! exists $args{econtext} or ! defined $args{econtext}) ||
-        (! exists $args{motherboard} or ! defined $args{motherboard}) ||
-        (! exists $args{mount_point} or ! defined $args{mount_point})) {
-        $errmsg = "EComponent::EWebserver::EApacge2->configureNode needs a motherboard, mount_point and econtext named argument!";
-        $log->error($errmsg);
-        throw Kanopya::Exception::Internal::IncorrectParam(error => $errmsg);
-    }
-    
-    my $config = {
-        INCLUDE_PATH => "/templates/components/apache2", #$self->_getEntity()->getTemplateDirectory(),
-        INTERPOLATE  => 1,               # expand "$var" in plain text
-        POST_CHOMP   => 0,               # cleanup whitespace 
-        EVAL_PERL    => 1,               # evaluate Perl code blocks
-        RELATIVE => 1,                   # desactive par defaut
-    };
-    
+    General::checkParams(args => \%args, required => ['econtext','motherboard','mount_point']);
+
     my $apache2_conf = $self->_getEntity()->getGeneralConf();    
-    $log->debug("Apache2 conf return is : " . Dumper($apache2_conf));
-    my $rand = new String::Random;
-    my $template = Template->new($config);
+    my $data = {};
     
     # generation of /etc/apache2/apache2.conf 
-    my $tmpfile = $rand->randpattern("cccccccc");
-    my $input = "apache2.conf.tt";
-    my $data = {};
     $data->{serverroot} = $apache2_conf->{'apache2_serverroot'};
-       
-    $template->process($input, $data, "/tmp/".$tmpfile) || do {
-        $errmsg = "EComponent::EWebserver::EApache2->addNode : error during template generation : $template->error;";
-        $log->error($errmsg);
-        throw Kanopya::Exception::Internal(error => $errmsg);    
-    };
-    $args{econtext}->send(src => "/tmp/$tmpfile", dest => $args{mount_point}.'/apache2/apache2.conf');    
-    unlink "/tmp/$tmpfile";
-    
-    # generation of /etc/apache2/ports.conf 
-    $tmpfile = $rand->randpattern("cccccccc");
-    $input = "ports.conf.tt";
+    $self->generateFile( econtext => $args{econtext}, mount_point => $args{mount_point},
+                         template_dir => "/templates/components/apache2",
+                         input_file => "apache2.conf.tt", output => "/apache2/apache2.conf", data => $data);
+
+    # generation of /etc/apache2/ports.conf
     $data = {};
     $data->{ports} = $apache2_conf->{apache2_ports};
     $data->{sslports} = $apache2_conf->{apache2_sslports};
-           
-    $template->process($input, $data, "/tmp/".$tmpfile) || do {
-        $errmsg = "EComponent::EWebserver::EApache2->addNode : error during template generation : $template->error;";
-        $log->error($errmsg);
-        throw Kanopya::Exception::Internal(error => $errmsg);    
-    };
-    $args{econtext}->send(src => "/tmp/$tmpfile", dest => $args{mount_point}.'/apache2/ports.conf');    
-    unlink "/tmp/$tmpfile";
     
+    $self->generateFile( econtext => $args{econtext}, mount_point => $args{mount_point},
+                         template_dir => "/templates/components/apache2",
+                         input_file => "ports.conf.tt", output => '/apache2/ports.conf', data => $data);
+        
     # generation of /etc/apache2/sites-available/default
-    $tmpfile = $rand->randpattern("cccccccc");
-    $input = "virtualhost.tt";
-    
     $data = {};
     $data->{virtualhosts} = $self->_getEntity()->getVirtualhostConf();
     $data->{ports} =  $apache2_conf->{apache2_ports};
     $data->{sslports} = $apache2_conf->{apache2_sslports};
     
-    $template->process($input, $data, "/tmp/".$tmpfile) || do {
-        $errmsg = "EComponent::EWebserver::EApache2->addNode : error during template generation : $template->error;";
-        $log->error($errmsg);
-        throw Kanopya::Exception::Internal(error => $errmsg);    
-    };
-    $args{econtext}->send(src => "/tmp/$tmpfile", dest => $args{mount_point}.'/apache2/sites-available/default');    
-    unlink "/tmp/$tmpfile";
+    $self->generateFile( econtext => $args{econtext}, mount_point => $args{mount_point},
+                         template_dir => "/templates/components/apache2",
+                         input_file => "virtualhost.tt", output => '/apache2/sites-available/default', data => $data);
+
+    # generation of /etc/apache2/mods-available/status.conf
+    $data = {};
+    $data->{monitor_server_ip} = 'all';
+    
+    $self->generateFile( econtext => $args{econtext}, mount_point => $args{mount_point},
+                         template_dir => "/templates/components/apache2",
+                         input_file => "status.conf.tt", output => '/apache2/mods-enabled/status.conf', data => $data);
+    
+    
     
     $self->addInitScripts(    etc_mountpoint => $args{mount_point}, 
                                 econtext => $args{econtext}, 
@@ -113,20 +85,14 @@ sub addNode {
     
 }
 
-sub removeNode{
-    
-}
+sub removeNode {}
 
-# Reload snmp process
 sub reload {
     my $self = shift;
     my %args = @_;
     
-    if(! exists $args{econtext} or ! defined $args{econtext}) {
-        $errmsg = "EComponent::EMonitoragent::ESnmpd5->reload needs an econtext named argument!";
-        $log->error($errmsg);
-        throw Kanopya::Exception::Internal::IncorrectParam(error => $errmsg);
-    }
+    General::checkParams(args => \%args, required => ['econtext']);
+
     my $command = "invoke-rc.d apache2 restart";
     my $result = $args{econtext}->execute(command => $command);
     return undef;
