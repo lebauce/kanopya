8d626e0128e97f4ce6c72063f5288c50cd12c8a9
Antoine Castaing <antoine.castaing@hederatech.com>
2011-06-07 10:25:32 +0200

    [Packaging] Change package builder, changelog and install scripts

--- lib/administrator/Entity/Component/Export/Iscsitarget1.pm
+++ lib/administrator/Entity/Component/Export/Iscsitarget1.pm
@@ -1,5 +1,5 @@
 # Iscsitarget1.pm -Ietd (iscsi target) 1 server component (Adminstrator side)
-#    Copyright © 2011 Hedera Technology SAS
+#    Copyright 2011 Hedera Technology SAS
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
--- tools/deployment/packaging/builder/package_builder.pl
+++ tools/deployment/packaging/builder/package_builder.pl
@@ -1,24 +1,45 @@
 #!/usr/bin/perl -W
+#    Copyright 2011 Hedera Technology SAS
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Maintained by Dev Team of Hedera Technology <dev@hederatech.com>.
+# Created 10 feb 2011
 
 #This script handle the generation of kanopya's packages (.deb)
-#@Author: Maxime <maxime.demoulin@hederatech.com>
-#@Date: 10/02/11
 #@Args: package's name
 #Conf: the package's conf file must contain path relatives to $orig_kanopya_dir
 #Please read the comments for more explanations
 
 use strict;
+use warnings;
 
-#version number is mandatory#
 if (!$ARGV[0]) {
-	print "usage: package_builder.pl <package name>, e.g: ./package_builder.pl lib.pl\n";
-	exit;
+    print "usage: package_builder.pl <package name>, e.g: ./package_builder.pl lib.pl\n";
+    exit;
 }
 
+############################# Init vars
 #package to build#
 my $package = $ARGV[0];
+
 #version of the package to build
 my $version;
+if ($ARGV[1]){
+    $version = $ARGV[1];
+    print "Version directly send by cmd line\n";
+}
+
 #dir where to build packages#
 my $temp_kanopya_dir = "/tmp/kanopya";
 #dir where is stored kanopya's source code#
@@ -32,67 +53,66 @@ my $kanopya_debian = $kanopya_packages.'/'.$package.'/debian';
 #print $kanopya_debian."\n";
 unless ( -d $temp_kanopya_dir ){mkdir $temp_kanopya_dir}
 #we prompt for version update
+
 print "Building $package \n";
 print "Do you want to update the changelog of the package? [y/n]: ";
 my $prompt = <STDIN>;
 chomp($prompt);
 if ($prompt eq 'y'){
-        print "Please be sure to respect the changelog convention as described on the Hedera's wiki\n";
-        chdir $kanopya_debian;
-        system('debchange --increment');
-        open(F,"$kanopya_debian/changelog");
-        my $line=<F>;close F;
-        #gather the full version (valid for ubuntu package and so forth)
-        #my $this='(\(\d*\.\d*.*\))';
-        #gather only version number, valid for Debian)
-        my $this='(\d*\.\d*)';
-        my @f=grep /$this/,$line;
-        $f[0] =~ $this;
-        $version = $1;
-	chdir $building_dir;
-}elsif ($prompt eq 'n'){
-        print "Old version will be kept \n";
-        open(F,"$kanopya_debian/changelog");
-        my $line=<F>;close F;
-        #gather the full version (valid for ubuntu package and so forth)
-        #my $this='(\(\d*\.\d*.*\))';
-        #gather only version number, valid for Debian)
-        my $this='(\d*\.\d*)';
-        my @f=grep /$this/,$line;
-        $f[0] =~ $this;
-        $version = $1;
+    if (! $version){
+        print "What is the version you want to publish ?\n";
+        $version = <STDIN>;
+        chomp($version);
+    }
+    print "VERSION IS $version";
+    print "Please be sure to respect the changelog convention as described on the Hedera's wiki\n";
+    chdir $kanopya_debian;
+    system('export DEBEMAIL="dev@hederatech.com"; export DEBFULLNAME="Hedera Dev Team" ;debchange --increment --distribution stable  --urgency low --check-dirname-level 0');
+#    system('debchange -v $version'."-1");
+    chdir $building_dir;
 }
+my $line;
+print "Open $kanopya_debian/changelog\n";
+open my $changelog,"<","$kanopya_debian/changelog";
+$line=<$changelog>;
+#
+print "LINE IS $line\n";
+my $this='(\d*\.\d*\.\d*)';
+my @f=grep /$this/,$line;
+$f[0] =~ $this;
+$version = $1;
+close $changelog;
+
 #package's directory#
 my $this_package = $temp_kanopya_dir."/".$package."-".$version;
-#print $this_package."\n";
+
 mkdir $this_package;
 #parse configuration file#
-open(FILE,"<$package.conf") or die "open: $!";
-my $line;
-LINE:
-while( defined( $line = <FILE> ) )
+open my $package_conf,"<", "$package.conf" or die "open: $!";
+
+while( $line = <$package_conf> )
 {
-	chomp ($line);
-	if ($line eq "none"){ last LINE; }
-	my $final = $this_package."".$line;
-	#we gather directories - lines ended by a "/" in the conf file#
-	if ( $final =~ m/\/$/){
-		system ("mkdir -p $final");
-		system ("cp -r $orig_kanopya_dir$line* $this_package$line");
-	#we gather files that are not directories#
-	#script is static and find files under sbin, conf, and scripts/init directories#
-	}else{
-		my @values = split('/', $line);
-		if ($values[1] eq "conf" || $values[1] eq "sbin"){
-			system ("mkdir -p $this_package/$values[1]");
-			system ("cp $orig_kanopya_dir$line $this_package/$values[1]/$values[2]");
-		}elsif ($values[1] eq "scripts"){
-			system ("mkdir -p $this_package/$values[2]");
-			system ("cp $orig_kanopya_dir$line $this_package/$values[2]/$values[3]");
-		}
-	}
+    chomp ($line);
+    my $final = $this_package."".$line;
+    #we gather directories - lines ended by a "/" in the conf file#
+    if ( $line =~ m/\/$/){
+        system ("mkdir -p $this_package$line");
+        system ("cp -r $orig_kanopya_dir$line* $this_package$line");
+    #we gather files that are not directories#
+    #script is static and find files under sbin, conf, and scripts/init directories#
+    }else{
+        my @values = split('/', $line);
+        my $file_nb = scalar @values -1;
+        my $filename = $values[$file_nb];
+        print "Copy file named <$filename> ";
+        delete $values[$file_nb];
+        my $dir = join('/',@values);
+        print "in relative directory <$dir>\n";
+        system ("mkdir -p $this_package/$dir");
+        system ("cp $orig_kanopya_dir$line $this_package/$dir/$filename");
+    }
 }
-close(FILE);
+close($package_conf);
 #we add debian directory to the package#
 system("cp -r $kanopya_debian $this_package/debian");
 
@@ -104,6 +124,7 @@ system ("debuild -us -uc");
 my $build_logs = $this_package."-logs";
 mkdir $build_logs;
 chdir $temp_kanopya_dir;
+
 system ('cp -r *.dsc *.build *.changes '.$ARGV[0].'_'.$version.'.tar.gz '.$build_logs);
 system ("tar czf $build_logs.tar.gz $build_logs");
 system ("rm -rf $build_logs");
