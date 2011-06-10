fd457d93c19b9f826d9c9358ed8a968b5fd25855
tortue <sylvain@hederatech.com>
2011-06-10 11:58:29 +0200

    Tool script to control all kanopya services

---
+++ tools/kanopya_services.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+# Call all kanopya init script with given param
+
+my @services = qw(executor state-manager collector grapher orchestrator);
+
+my $cmd = $ARGV[0] or die "Need param start/stop/restart/status";
+
+for my $srv (@services) {
+    system "invoke-rc.d kanopya-$srv $cmd";
+}
