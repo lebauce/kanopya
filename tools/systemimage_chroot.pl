7c194ca0a89322c3613e8c61d7bb55a78d5e6467
Thomas MANNI <thomas.manni@hederatech.com>
2011-06-22 13:58:31 +0200

    [tools] systemimage_chroot now kill processes launched from chroot environment before unmounting devices

--- tools/systemimage_chroot.pl
+++ tools/systemimage_chroot.pl
@@ -14,6 +14,11 @@ if($currentuser ne 'root') {
     die("You must be root to use this script.\n");
 }
 
+my $lsof = `which fuser`;
+if(!$lsof) {
+    die("fuser command not found but required !\n");  
+}
+
 my ($sysimg_name) = @ARGV;
 if(not defined $sysimg_name) {
 	print "We must specify systemimage name to chroot ; this systemimage must be deactivated\n";
@@ -68,18 +73,6 @@ print "#######################################################\n";
 print "   :: System Image <$sysimg_name> Customization ::\n";
 print "#######################################################\n";
 
-# first we check if no iscsi connection exists
-#my $ret = `cat /proc/net/iet/session`;
-#$ret =~ m/tid:([0-9]+)\sname:$root_target\n(\tsid:[0-9]+\sinitiator:.*\n\t\tcid:.*\n)*/;
-#if( defined $2 ) {
-#    print "Iscsi session exists for $root_target\n";
-#}
-
-#$ret =~ m/tid:([0-9]+)\sname:$datadisk->{target}\n(\tsid:[0-9]+\sinitiator:.*\n\t\tcid:.*\n)*/;
-#if( defined $2 ) {
-#    print "Iscsi session exists for $datadisk->{target} \n";
-#}
-
 # we mount all devices
 
 if(! -e '/mnt/chroot') { 
@@ -99,10 +92,6 @@ print "mounting $etc_device on /mnt/chroot/etc...";
 $out = `mount $etc_device /mnt/chroot/etc`;
 print "done\n";
 
-#print "mounting $datadisk->{device} on /mnt/chroot/srv...";
-#$out = `mount $datadisk->{device} /mnt/chroot/srv`;
-#print "done\n";
-
 print "mounting procsfs on /mnt/chroot/proc...";
 $out = `mount -t proc proc /mnt/chroot/proc`;
 print "done\n";
@@ -117,10 +106,38 @@ print "done\n";
 
 $out = `echo $sysimg_name > /mnt/chroot/etc/debian_chroot`;
 
-
-
 # we chroot in the new environment 
 print "starting the new environment\n";
-exec("/usr/sbin/chroot /mnt/chroot /bin/bash ;
-      umount /mnt/chroot/etc /mnt/chroot/dev/pts /mnt/chroot/proc /mnt/chroot/sys ; 
-      umount /mnt/chroot");
+system("/usr/sbin/chroot /mnt/chroot /bin/bash"); 
+
+# after exiting the chroot...      
+print "flush file system buffers\n";
+system('sync');
+
+print "kill processes launched from chroot environment... ";
+$out = `fuser -skm /mnt/chroot`;
+print "done\n";
+ 
+print "umount devpts... ";
+$out = `umount /mnt/chroot/dev/pts`;
+print "done\n";
+
+print "umount sysfs... ";
+$out = `umount /mnt/chroot/sys`;
+print "done\n";
+
+print "umount procfs... ";
+$out = `umount /mnt/chroot/proc`;
+print "done\n";
+
+print "umount etc container... ";
+$out = `umount /mnt/chroot/etc`;
+print "done\n";
+
+print "umount root container...";
+$out = `umount /mnt/chroot`;
+print "done\n";
+
+
+
+   
