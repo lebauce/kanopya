2ed730ebf95a2d844ea3d3a18a99c0b120311da6
Hobbestigrou <natal@hederatech.com>
2011-09-20 17:52:54 +0200

    [Front] Frontend: Move login route hanlder in login class, and load the login class.

--- ui/Frontend/lib/Frontend.pm
+++ ui/Frontend/lib/Frontend.pm
@@ -1,14 +1,16 @@
 package Frontend;
 use Dancer ':syntax';
 
+use Login;
+
+use Log::Log4perl;
+
+Log::Log4perl->init('/opt/kanopya/conf/webui-log.conf');
+
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index';
-};
-
-get '/login' => sub {
-    template 'login', {},{ layout=>'login' };
 };
 
 true;
