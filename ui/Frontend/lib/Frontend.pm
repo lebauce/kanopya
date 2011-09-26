960ac9ca18151dac24d45fbdfd884c10cd018b52
Hobbestigrou <natal@hederatech.com>
2011-09-26 17:56:15 +0200

    [Front] Frotend: If the use is not login, set eid env var and instancie the adminstrator object.

--- ui/Frontend/lib/Frontend.pm
+++ ui/Frontend/lib/Frontend.pm
@@ -11,9 +11,20 @@ our $VERSION = '0.1';
 Log::Log4perl->init('/opt/kanopya/conf/webui-log.conf');
 
 before sub {
+    my $eid = session('EID');
+
     if ( ! session('username') && request->path ne '/login' ) {
         return '/login';
     }
+    elsif ( request->path eq '/login' ) {
+        return pass;
+    }
+    else {
+        $ENV{EID}      = $eid;
+        var adm_object => Administrator->new();
+        return pass;
+    }
+
 };
 
 get '/' => sub {
