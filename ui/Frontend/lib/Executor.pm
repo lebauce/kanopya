69fd3c678c78a2d456c1267e3c4753c12e41c0e2
Carlos Sosa <gnusosa@gnusosa.net>
2011-09-29 00:40:40 -0700

    Added ../lib/Executor.pm and modified Clusters.pm and Distributions.pm. Small details.

---
+++ ui/Frontend/lib/Executor.pm
@@ -0,0 +1,14 @@
+package Executor;
+
+use Dancer ':syntax'; 
+
+use Log::Log4perl "get_logger";
+my $log = get_logger("webui");
+
+get "/execution" => sub { 
+    template 'execution', {
+        title_page         => 'Dashboard - Operations queue',
+		username 		   => session('username'), 
+        object             => vars->{adm_object}->getOperations(),
+    };
+}
