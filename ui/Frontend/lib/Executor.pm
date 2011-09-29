fc5a17fc202db145f910635ed98b38fb5cae9c09
Hobbestigrou <natal@hederatech.com>
2011-09-29 10:48:06 +0200

    [Front] Executor: Remplace all tab by space.

--- ui/Frontend/lib/Executor.pm
+++ ui/Frontend/lib/Executor.pm
@@ -1,14 +1,14 @@
 package Executor;
 
-use Dancer ':syntax'; 
+use Dancer ':syntax';
 
 use Log::Log4perl "get_logger";
 my $log = get_logger("webui");
 
-get "/execution" => sub { 
+get "/execution" => sub {
     template 'execution', {
-        title_page         => 'Dashboard - Operations queue',
-		username 		   => session('username'), 
-        object             => vars->{adm_object}->getOperations(),
+        title_page  => 'Dashboard - Operations queue',
+        username    => session('username'),
+        object      => vars->{adm_object}->getOperations(),
     };
 }
