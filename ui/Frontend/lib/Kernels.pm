f953e3a9cd27af33bfd2a7c8ba49e1ac454b0b02
Hobbestigrou <natal@hederatech.com>
2011-10-04 10:29:37 +0200

    [Front] Kernels: Clean code.

--- ui/Frontend/lib/Kernels.pm
+++ ui/Frontend/lib/Kernels.pm
@@ -10,16 +10,17 @@ sub _view_kernels {
 
     my @ekernels = Entity::Kernel->getKernels(hash => {});
     my $kernels = [];
-    
+
     foreach my $m (@ekernels) {
-        my $tmp = {};
-        my $methods = $m->getPerms();
-        $tmp->{kernel_id} = $m->getAttr(name => 'kernel_id');
-        $tmp->{kernel_name} = $m->getAttr(name => 'kernel_name');
+        my $tmp      = {};
+        my $methods  = $m->getPerms();
+
+        $tmp->{kernel_id}      = $m->getAttr(name => 'kernel_id');
+        $tmp->{kernel_name}    = $m->getAttr(name => 'kernel_name');
         $tmp->{kernel_version} = $m->getAttr(name => 'kernel_version');
-        $tmp->{kernel_desc} = $m->getAttr(name => 'kernel_desc');
-        if($methods->{'setperm'}->{'granted'}) { $tmp->{can_setperm} = 1; }
-        
+        $tmp->{kernel_desc}    = $m->getAttr(name => 'kernel_desc');
+        $tmp->{can_setperm}    = 1 if ( $methods->{'setperm'}->{'granted'} );
+
         push (@$kernels, $tmp);
     }
 
@@ -27,18 +28,16 @@ sub _view_kernels {
 }
 
 get "/kernels" => sub {
-    my $link_upload;
 
-    my $methods = Entity::Kernel->getPerms();
-    if($methods->{'upload'}->{'granted'}) { $link_upload = 1 }
-    else { $link_upload = 0 }
+    my $methods     = Entity::Kernel->getPerms();
+    my $link_upload = $methods->{'upload'}->{'granted'} ? 1 : 0;
 
     template 'kernel', {
-    kernels_list => _view_kernels(),
-    titlepage => 'Systems - Kernels',
-    username => session('username'),
-    link_upload => $link_upload,
-    }; 
+        kernels_list => _view_kernels(),
+        titlepage    => 'Systems - Kernels',
+        username     => session('username'),
+        link_upload  => $link_upload,
+    };
 }
 
 
