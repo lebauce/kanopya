ba41506304f9fa9649aac56922dd1d03bc6e58a9
Thomas MANNI <thomas.manni@hederatech.com>
2011-10-13 18:47:46 +0200

    [front] hosts work in progress

---
+++ ui/Frontend/lib/Hosts.pm
@@ -0,0 +1,26 @@
+package Hosts;
+
+use Dancer ':syntax';
+
+use Administrator;
+use Entity::Motherboard;
+use Entity::Kernel;
+use Entity::Cluster;
+use Entity::Processormodel;
+use Entity::Motherboardmodel;
+use Entity::Powersupplycard;
+use Log::Log4perl "get_logger";
+
+my $log = get_logger("webui");
+
+prefix '/infrastructures';
+
+sub _hosts {
+
+}
+
+sub 
+
+
+
+1;
