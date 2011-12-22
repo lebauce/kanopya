08aa6470b1e963551915ee84ed072e385a8f0568
Antoine Castaing <antoine.castaing@hederatech.com>
2011-12-22 10:01:36 +0100

    [Core] First version of the EVirtual EHost

---
+++ lib/executor/EEntity/EHost/EVirtual.pm
@@ -0,0 +1,45 @@
+# EVirtual.pm - class of virtual EHosts object
+
+#    Copyright © 2011 Hedera Technology SAS
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Maintained by Dev Team of Hedera Technology <dev@hederatech.com>.
+# Created 20 nov 2011
+
+=head1 NAME
+
+EVirtual - execution class of virtual host entities
+
+=head1 SYNOPSIS
+
+
+
+=head1 DESCRIPTION
+
+EHost::EVirtual is the execution class for virtual host entities
+
+=head1 METHODS
+
+=cut
+package EEntity::EHost::EVirtual;
+use base "EEntity::EHost";
+
+sub start{
+    
+}
+
+sub stop {
+    
+}
+1;
