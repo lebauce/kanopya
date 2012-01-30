1f7303a033acdf78e519c5811fb52c17ed8c5097
Thomas MANNI <thomas.manni@hederatech.com>
2012-01-30 14:01:11 +0100

    [sql] bug fix

--- scripts/database/mysql/schemas/components/mysql5.sql
+++ scripts/database/mysql/schemas/components/mysql5.sql
@@ -7,8 +7,7 @@ SET foreign_key_checks=0;
 --
 
 CREATE TABLE `mysql5` (
-  `mysql5_id` int(8) unsigned NOT NULL AUTO_INCREMENT,
-  `component_instance_id` int(8) unsigned NOT NULL,
+  `mysql5_id` int(8) unsigned NOT NULL,
   `mysql5_port` int(2) unsigned NOT NULL DEFAULT 3306,
   `mysql5_datadir` char(64) NOT NULL DEFAULT '/var/lib/mysql',
   `mysql5_bindaddress` char(17) NOT NULL DEFAULT '127.0.0.1', 
