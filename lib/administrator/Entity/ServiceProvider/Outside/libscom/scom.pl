83364c62a6a962947fe0b14dd63bd5b1e492fdf5
tortue <sylvain@hederatech.com>
2012-02-06 19:13:27 +0100

    [scom] comments and remove tabulations

--- lib/administrator/Entity/ServiceProvider/Outside/libscom/scom.pl
+++ lib/administrator/Entity/ServiceProvider/Outside/libscom/scom.pl
@@ -1,12 +1,13 @@
+# ScomQuery sample/test script
 use ScomQuery;
 use Data::Dumper;
 
 my $management_server_name = "WIN-09DSUKS61DT.hedera.forest";
 
 my %counters = (
-	'Memory' 	=> ['Available MBytes','PercentMemoryUsed'],
-	'Process' 	=> ['% Processor Time'],
-	'Processor' => ['% Processor Time'],
+    'Memory'    => ['Available MBytes','PercentMemoryUsed'],
+    'Process'   => ['% Processor Time'],
+    'Processor' => ['% Processor Time'],
 );
 
 my $start_time = '2/2/2012 11:00:00 AM';
@@ -15,9 +16,9 @@ my $end_time = '2/2/2012 12:00:00 PM';
 my $scom = ScomQuery->new( server_name => $management_server_name );
 
 my $res = $scom->getPerformance(
-	counters => \%all_counters,
-	start_time => $start_time,
-	end_time => $end_time,
+    counters => \%counters,
+    start_time => $start_time,
+    end_time => $end_time,
 );
 
 print Dumper $res;
