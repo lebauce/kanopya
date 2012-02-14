bb9d62815aaa8d47c67fbe2fadc4c3edba6d5b0e
tgenin <tgenin@hedera-technology.com>
2012-02-14 16:22:18 +0100

    [Agg] rename AggregateCondition

--- t/orchestrator/aggregate_rule.pl
+++ t/orchestrator/aggregate_rule.pl
@@ -1,29 +1,29 @@
 use lib qw(/opt/kanopya/lib/administrator /opt/kanopya/lib/monitor /opt/kanopya/lib/orchestrator /opt/kanopya/lib/common);
 use Administrator;
 use General;
-use AggregateRule;
+use AggregateCondition;
 
-use Orchestrator;
+#use Log::Log4perl qw(:easy);
+#Log::Log4perl->easy_init({level=>'DEBUG', file=>'STDOUT', layout=>'%F %L %p %m%n'});
 
 Administrator::authenticate( login =>'admin', password => 'K4n0pY4' );
 my $adm = Administrator->new();
 
 $params = {
-    rule_id               => 3,
     aggregate_id          => 1,
     comparator            => '<',
     threshold             => 0.1,
     state                 => 'enabled',
     time_limit            => NULL,
-    last_eval             => NULL,
 };
 
-my $aggregate_rule = AggregateRule->new(%$params);
+#my $aggregate_rule = AggregateRule->new(%$params);
 
-my @aggregate_rules = AggregateRule->search(hash => {});
-for my $aggregate_rule (@aggregate_rules){
-    print $aggregate_rule->toString()."\n";
-    $aggregate_rule->eval();
+my @aggregate_conditions = AggregateCondition->search(hash => {});
+for my $aggregate_condition (@aggregate_conditions){
+    #print $aggregate_condition->toString()."\n";
+    $aggregate_condition->eval();
+    
 }
 
 
