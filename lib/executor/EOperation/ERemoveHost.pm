3df1775b0f8e8f99df410199f9b72500bed63d06
Kevin Pouget <kevin.pouget@hederatech.com>
2012-02-23 14:54:55 +0100

    [core] Removed useless reference to Kanopya nas from operations.

--- lib/executor/EOperation/ERemoveHost.pm
+++ lib/executor/EOperation/ERemoveHost.pm
@@ -51,51 +51,18 @@ my $log = get_logger("executor");
 my $errmsg;
 our $VERSION = '1.00';
 
-=head2 new
-
-    my $op = EEntity::EOperation::ERemoveHost->new();
-
-EEntity::Operation::ERemoveHost->new creates a new RemoveMotheboard operation.
-
-=cut
-
-sub new {
-    my $class = shift;
-    my %args = @_;
-    
-    $log->debug("Class is : $class");
-    my $self = $class->SUPER::new(%args);
-    $self->_init();
-    
-    return $self;
-}
-
-=head2 _init
-
-    $op->_init() is a private method used to define internal parameters.
-
-=cut
-
-sub _init {
-    my $self = shift;
-
-    return;
-}
-
 sub checkOp{
     my $self = shift;
     my %args = @_;
     
     # check if host is not active
     $log->debug("checking host active value <$args{params}->{host_id}>");
-       if($self->{_objs}->{host}->getAttr(name => 'active')) {
-            $errmsg = "EOperation::ERemoveHost->prepare : host $args{params}->{host_id} is still active";
-            $log->error($errmsg);
-            throw Kanopya::Exception::Internal(error => $errmsg);
+    if($self->{_objs}->{host}->getAttr(name => 'active')) {
+        $errmsg = "EOperation::ERemoveHost->prepare : host $args{params}->{host_id} is still active";
+        $log->error($errmsg);
+        throw Kanopya::Exception::Internal(error => $errmsg);
     }
-
 }
-
 
 =head2 prepare
 
@@ -108,16 +75,11 @@ sub prepare {
     my %args = @_;
     $self->SUPER::prepare();
 
-    if ((! exists $args{internal_cluster} or ! defined $args{internal_cluster})) { 
-        $errmsg = "ERemoveHost->prepare need an internal_cluster named argument!";
-        $log->error($errmsg);
-        throw Kanopya::Exception::Internal::IncorrectParam(error => $errmsg);
-    }
-    $log->debug("After Eoperation prepare and before get Administrator singleton");
+    General::checkParams(args => \%args, required => ["internal_cluster"]);
+
     my $params = $self->_getOperation()->getParams();
 
     $self->{_objs} = {};
-    $self->{nas} = {};
     $self->{executor} = {};
 
     # Instantiate host and so check if exists
@@ -140,45 +102,20 @@ sub prepare {
         $log->error($errmsg);
         throw Kanopya::Exception::Internal::WrongValue(error => $errmsg);
     }
-
-    ## Instanciate Clusters
-    # Instanciate nas Cluster 
-    $self->{nas}->{obj} = Entity::ServiceProvider::Inside::Cluster->get(id => $args{internal_cluster}->{nas});
-    # Instanciate executor Cluster
-    $self->{executor}->{obj} = Entity::ServiceProvider::Inside::Cluster->get(id => $args{internal_cluster}->{executor});
-
-    ## Get Internal IP
-    # Get Internal Ip address of Master node of cluster Executor
-    my $exec_ip = $self->{executor}->{obj}->getMasterNodeIp();
-    # Get Internal Ip address of Master node of cluster nas
-    my $nas_ip = $self->{nas}->{obj}->getMasterNodeIp();
-    
-    
-    ## Instanciate context 
-    # Get context for nas
-    $self->{nas}->{econtext} = EFactory::newEContext(ip_source => $exec_ip, ip_destination => $nas_ip);
-        
-    ## Instanciate Component needed (here LVM and ISCSITARGET on nas cluster)
-    # Instanciate Cluster Storage component.
-    my $tmp = $self->{nas}->{obj}->getComponent(name=>"Lvm",
-                                         version => "2");
-    $log->debug("Value return by getcomponent ". ref($tmp));
-    $self->{_objs}->{component_storage} = EFactory::newEEntity(data => $tmp);
-    
 }
 
 sub execute{
     my $self = shift;
     $self->SUPER::execute();
-    my ($powersupplycard,$powersupplyid);
+    my ($powersupplycard, $powersupplyid);
 
     my $powersupplycard_id = $self->{_objs}->{host}->getPowerSupplyCardId();
     if ($powersupplycard_id) {
         $powersupplycard = Entity::Powersupplycard(id => $powersupplycard_id);
         $powersupplyid = $self->{_objs}->{host}->getAttr(name => 'host_powersupply_id');
     }
-    $self->{_objs}->{component_storage}->removeDisk(name => $self->{_objs}->{host}->getEtcName(), econtext => $self->{nas}->{econtext});
     $self->{_objs}->{host}->delete();
+
     if ($powersupplycard_id){
         $log->debug("Deleting powersupply with id <$powersupplyid> on the card : <$powersupplycard>");
         $powersupplycard->delPowerSupply(powersupply_id => $powersupplyid);
