09848b9e716d88e064a1d4dc32ece08ee52c5cd1
tgenin <tgenin@hedera-technology.com>
2012-03-01 11:45:40 +0100

    [ui] Cluster metrics creation and deletion

--- ui/Frontend/lib/Monitoring.pm
+++ ui/Frontend/lib/Monitoring.pm
@@ -290,6 +290,48 @@ get '/clustermetrics' => sub {
       };
 };
 
+get '/clustermetrics/new' => sub {
+    
+   my $cluster_id    = params->{extclusterid} || 0;
+   
+    my $adm    = Administrator->new();
+    my $scom_indicatorset = $adm->{'manager'}{'monitor'}->getSetDesc( set_name => 'scom' );
+    my @indicators;
+    
+    foreach my $indicator (@{$scom_indicatorset->{ds}}){
+        my $hash = {
+            id     => $indicator->{id},
+            label  => $indicator->{label},
+        };
+
+        push @indicators, $hash;
+    }
+
+   
+    template 'clustermetric_new', {
+        title_page => "Clustermetric creation",
+        indicators => \@indicators,
+    };
+};
+
+
+post '/clustermetrics/new' => sub {
+    my $cm_params = {
+        clustermetric_cluster_id               => '54',
+        clustermetric_indicator_id             => param('id2'),
+        clustermetric_statistics_function_name => param('function'),
+        clustermetric_window_time              => '1200',
+    };
+   my $cm = Clustermetric->new(%$cm_params);
+   redirect("/architectures/clustermetrics");
+};
+
+get '/clustermetrics/:clustermetricid/delete' => sub {
+    my $clustermetric = Clustermetric->get('id' => params->{clustermetricid});
+    $clustermetric->delete();
+    redirect('/architectures/clustermetrics');
+};
+
 get '/rules' => sub {
   my @enabled_aggregaterules = AggregateRule->getRules(state => 'enabled'); 
 #  my @enabled_aggregaterules = AggregateRule->search(hash => {aggregate_rule_state => 'enabled'});
