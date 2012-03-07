b37975e254cb9454cd1765b1b774c8c4ca69f7fb
tgenin <tgenin@hedera-technology.com>
2012-03-07 15:38:42 +0100

    [db] change names

--- scripts/database/mysql/schemas/Schemas.sql
+++ scripts/database/mysql/schemas/Schemas.sql
@@ -1221,13 +1221,13 @@ CREATE TABLE `nodemetric_rule` (
 --
 
 CREATE TABLE `verified_noderule` (
-  `verified_rule_externalnode_id` int(8) unsigned NOT NULL,
-  `verified_rule_nodemetric_rule_id` int(8) unsigned NOT NULL,
-  PRIMARY KEY (`verified_rule_externalnode_id`,`verified_rule_nodemetric_rule_id`),
-  KEY (`verified_rule_nodemetric_rule_id`),
-  FOREIGN KEY (`verified_rule_nodemetric_rule_id`) REFERENCES `nodemetric_rule` (`nodemetric_rule_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  KEY(`verified_rule_externalnode_id`),
-  FOREIGN KEY (`verified_rule_externalnode_id`) REFERENCES `externalnode` (`externalnode_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `verified_noderule_externalnode_id` int(8) unsigned NOT NULL,
+  `verified_noderule_nodemetric_rule_id` int(8) unsigned NOT NULL,
+  PRIMARY KEY (`verified_noderule_externalnode_id`,`verified_noderule_nodemetric_rule_id`),
+  KEY (`verified_noderule_nodemetric_rule_id`),
+  FOREIGN KEY (`verified_noderule_nodemetric_rule_id`) REFERENCES `nodemetric_rule` (`nodemetric_rule_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  KEY(`verified_noderule_externalnode_id`),
+  FOREIGN KEY (`verified_noderule_externalnode_id`) REFERENCES `externalnode` (`externalnode_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB  DEFAULT CHARSET=utf8;
 
 
