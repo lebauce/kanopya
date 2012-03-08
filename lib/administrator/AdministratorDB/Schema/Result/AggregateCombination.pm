5bae778d43f577178772a9dc756c05fdbb90da9f
tortue <sylvain@hederatech.com>
2012-03-08 10:48:19 +0100

    [schema] update schema to integrate metric/rule links to service provider

--- lib/administrator/AdministratorDB/Schema/Result/AggregateCombination.pm
+++ lib/administrator/AdministratorDB/Schema/Result/AggregateCombination.pm
@@ -22,22 +22,50 @@ __PACKAGE__->table("aggregate_combination");
 =head2 aggregate_combination_id
 
   data_type: 'integer'
+  extra: {unsigned => 1}
   is_auto_increment: 1
+  is_nullable: 0
+
+=head2 aggregate_combination_service_provider_id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_foreign_key: 1
   is_nullable: 0
 
 =head2 aggregate_combination_formula
 
-  data_type: 'text'
+  data_type: 'char'
+  is_nullable: 0
+  size: 32
+
+=head2 class_type_id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_foreign_key: 1
   is_nullable: 0
 
 =cut
 
 __PACKAGE__->add_columns(
   "aggregate_combination_id",
-  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "aggregate_combination_service_provider_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 0,
+  },
   "aggregate_combination_formula",
-  { data_type => "text", is_nullable => 0 },
-      "class_type_id",
+  { data_type => "char", is_nullable => 0, size => 32 },
+  "class_type_id",
   {
     data_type => "integer",
     extra => { unsigned => 1 },
@@ -47,9 +75,60 @@ __PACKAGE__->add_columns(
 );
 __PACKAGE__->set_primary_key("aggregate_combination_id");
 
+=head1 RELATIONS
 
-# Created by DBIx::Class::Schema::Loader v0.07002 @ 2012-02-15 18:00:34
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:tLyHmT6MCY8PuKiMUZPZng
+=head2 aggregate_combination_service_provider
+
+Type: belongs_to
+
+Related object: L<AdministratorDB::Schema::Result::ServiceProvider>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "aggregate_combination_service_provider",
+  "AdministratorDB::Schema::Result::ServiceProvider",
+  {
+    service_provider_id => "aggregate_combination_service_provider_id",
+  },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+=head2 class_type
+
+Type: belongs_to
+
+Related object: L<AdministratorDB::Schema::Result::ClassType>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "class_type",
+  "AdministratorDB::Schema::Result::ClassType",
+  { class_type_id => "class_type_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+=head2 aggregate_conditions
+
+Type: has_many
+
+Related object: L<AdministratorDB::Schema::Result::AggregateCondition>
+
+=cut
+
+__PACKAGE__->has_many(
+  "aggregate_conditions",
+  "AdministratorDB::Schema::Result::AggregateCondition",
+  {
+    "foreign.aggregate_combination_id" => "self.aggregate_combination_id",
+  },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07002 @ 2012-03-08 10:27:11
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:8AJHqC7offsQqngvtBXvsw
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
--- lib/administrator/AdministratorDB/Schema/Result/AggregateRule.pm
+++ lib/administrator/AdministratorDB/Schema/Result/AggregateRule.pm
@@ -22,50 +22,82 @@ __PACKAGE__->table("aggregate_rule");
 =head2 aggregate_rule_id
 
   data_type: 'integer'
+  extra: {unsigned => 1}
   is_auto_increment: 1
+  is_nullable: 0
+
+=head2 aggregate_rule_service_provider_id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_foreign_key: 1
   is_nullable: 0
 
 =head2 aggregate_rule_formula
 
-  data_type: 'text'
+  data_type: 'char'
   is_nullable: 0
+  size: 32
 
 =head2 aggregate_rule_last_eval
 
   data_type: 'integer'
+  extra: {unsigned => 1}
   is_nullable: 1
 
 =head2 aggregate_rule_timestamp
 
   data_type: 'integer'
+  extra: {unsigned => 1}
   is_nullable: 1
 
 =head2 aggregate_rule_state
 
-  data_type: 'text'
+  data_type: 'char'
   is_nullable: 0
+  size: 32
 
 =head2 aggregate_rule_action_id
 
   data_type: 'integer'
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+=head2 class_type_id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_foreign_key: 1
   is_nullable: 0
 
 =cut
 
 __PACKAGE__->add_columns(
   "aggregate_rule_id",
-  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "aggregate_rule_service_provider_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 0,
+  },
   "aggregate_rule_formula",
-  { data_type => "text", is_nullable => 0 },
+  { data_type => "char", is_nullable => 0, size => 32 },
   "aggregate_rule_last_eval",
-  { data_type => "integer", is_nullable => 1 },
+  { data_type => "integer", extra => { unsigned => 1 }, is_nullable => 1 },
   "aggregate_rule_timestamp",
-  { data_type => "integer", is_nullable => 1 },
+  { data_type => "integer", extra => { unsigned => 1 }, is_nullable => 1 },
   "aggregate_rule_state",
-  { data_type => "text", is_nullable => 0 },
+  { data_type => "char", is_nullable => 0, size => 32 },
   "aggregate_rule_action_id",
-  { data_type => "integer", is_nullable => 0 },
-      "class_type_id",
+  { data_type => "integer", extra => { unsigned => 1 }, is_nullable => 0 },
+  "class_type_id",
   {
     data_type => "integer",
     extra => { unsigned => 1 },
@@ -75,9 +107,41 @@ __PACKAGE__->add_columns(
 );
 __PACKAGE__->set_primary_key("aggregate_rule_id");
 
+=head1 RELATIONS
 
-# Created by DBIx::Class::Schema::Loader v0.07002 @ 2012-02-15 13:58:53
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:doTpkeVMPAOe1v9NT7zJPA
+=head2 aggregate_rule_service_provider
+
+Type: belongs_to
+
+Related object: L<AdministratorDB::Schema::Result::ServiceProvider>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "aggregate_rule_service_provider",
+  "AdministratorDB::Schema::Result::ServiceProvider",
+  { service_provider_id => "aggregate_rule_service_provider_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+=head2 class_type
+
+Type: belongs_to
+
+Related object: L<AdministratorDB::Schema::Result::ClassType>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "class_type",
+  "AdministratorDB::Schema::Result::ClassType",
+  { class_type_id => "class_type_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07002 @ 2012-03-08 10:27:11
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:MOHNlpa7wVQM2c2Sv8ixGw
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
