e7bbb836949e8516d6ea5f41b425c7044855c58b
maxime <mdemoulin@hedera-technology.com>
2012-04-24 16:02:05 +0200

    [Collector] add of primary files for new component kanopya collector

--- scripts/database/mysql/schemas/components/kanopyacollector1.sql
+++ scripts/database/mysql/schemas/components/kanopyacollector1.sql
@@ -10,6 +10,7 @@ CREATE TABLE `kanopyacollector1` (
     `kanopyacollector1_id` int(8) unsigned NOT NULL,
     `collect_frequency` int unsigned NOT NULL DEFAULT 3600,
     `storage_time` int unsigned NOT NULL DEFAULT 86400,
+    PRIMARY KEY (`kanopyacollector1_id`),
     CONSTRAINT FOREIGN KEY (`kanopyacollector1_id`) REFERENCES `component` (`component_id`) ON DELETE CASCADE ON UPDATE NO ACTION
 )   ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
