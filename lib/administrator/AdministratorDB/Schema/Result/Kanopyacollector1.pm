1c92af17019fb4e2bc2c84ef7316752297c476de
maxime <mdemoulin@hedera-technology.com>
2012-04-24 16:02:05 +0200

    [Collector] add of primary files for new component kanopya collector

---
+++ lib/administrator/AdministratorDB/Schema/Result/Kanopyacollector1.pm
@@ -0,0 +1,105 @@
+use utf8;
+package AdministratorDB::Schema::Result::Kanopyacollector1;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+AdministratorDB::Schema::Result::Kanopyacollector1
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<kanopyacollector1>
+
+=cut
+
+__PACKAGE__->table("kanopyacollector1");
+
+=head1 ACCESSORS
+
+=head2 kanopyacollector1_id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 collect_frequency
+
+  data_type: 'integer'
+  default_value: 3600
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+=head2 storage_time
+
+  data_type: 'integer'
+  default_value: 86400
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "kanopyacollector1_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_foreign_key => 1,
+    is_nullable => 0,
+  },
+  "collect_frequency",
+  {
+    data_type => "integer",
+    default_value => 3600,
+    extra => { unsigned => 1 },
+    is_nullable => 0,
+  },
+  "storage_time",
+  {
+    data_type => "integer",
+    default_value => 86400,
+    extra => { unsigned => 1 },
+    is_nullable => 0,
+  },
+);
+
+=head1 RELATIONS
+
+=head2 kanopyacollector1
+
+Type: belongs_to
+
+Related object: L<AdministratorDB::Schema::Result::Component>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "kanopyacollector1",
+  "AdministratorDB::Schema::Result::Component",
+  { component_id => "kanopyacollector1_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07022 @ 2012-04-24 19:08:47
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:C9TGy65jgpHaUndlw+WfGQ
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+__PACKAGE__->belongs_to(
+  "parent",
+  "AdministratorDB::Schema::Result::Component",
+    { "foreign.component_id" => "self.kanopyacollector1_id" },
+    { cascade_copy => 0, cascade_delete => 1 }
+);
+
+
+
+1;
---
+++ scripts/database/mysql/schemas/components/kanopyacollector1.sql
@@ -0,0 +1,17 @@
+USE `kanopya`;
+
+SET foreign_key_checks=0;
+
+--
+-- Table structure for table `kanopyacollector1`
+--
+
+CREATE TABLE `kanopyacollector1` (
+    `kanopyacollector1_id` int(8) unsigned NOT NULL,
+    `collect_frequency` int unsigned NOT NULL DEFAULT 3600,
+    `storage_time` int unsigned NOT NULL DEFAULT 86400,
+    PRIMARY KEY (`kanopyacollector1_id`),
+    CONSTRAINT FOREIGN KEY (`kanopyacollector1_id`) REFERENCES `component` (`component_id`) ON DELETE CASCADE ON UPDATE NO ACTION
+)   ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET foreign_key_checks=1;
