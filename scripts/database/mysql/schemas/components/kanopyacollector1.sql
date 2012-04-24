126fafc5b696d98443a544df23780b313adb23da
maxime <mdemoulin@hedera-technology.com>
2012-04-24 16:32:56 +0200

    [collector] sql cannot be put out the main data.sql.tt due to the use of @eid variable

--- scripts/database/mysql/schemas/components/kanopyacollector1.sql
+++ scripts/database/mysql/schemas/components/kanopyacollector1.sql
@@ -10,7 +10,7 @@ CREATE TABLE `kanopyacollector1` (
     `kanopyacollector1_id` int(8) unsigned NOT NULL,
     `collect_frequency` int unsigned NOT NULL DEFAULT 3600,
     `storage_time` int unsigned NOT NULL DEFAULT 86400,
-    CONSTRAINT FOREIGN KEY (`kanopyacollector_id`) REFERENCES `component` (`component_id`) ON DELETE CASCADE ON UPDATE NO ACTION
+    CONSTRAINT FOREIGN KEY (`kanopyacollector1_id`) REFERENCES `component` (`component_id`) ON DELETE CASCADE ON UPDATE NO ACTION
 )   ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET foreign_key_checks=1;
