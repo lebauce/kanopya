88fc5a6b4763c6eceab279f368f9d28d3b5241b1
maxime <mdemoulin@hedera-technology.com>
2012-04-24 16:02:05 +0200

    [Collector] add of primary files for new component kanopya collector

---
+++ lib/administrator/CollectorManager.pm
@@ -0,0 +1,29 @@
+# CollectorManager.pm - Object class of Collector Manager included in Administrator
+
+#    Copyright © 2011 Hedera Technology SAS
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Maintained by Dev Team of Hedera Technology <dev@hederatech.com>.
+# Created 20 april 2012
+
+package CollectorManager;
+
+use strict;
+use warnings;
+use Log::Log4perl "get_logger";
+use Data::Dumper;
+use Kanopya::Exceptions;
+use General;
+
+my $log = get_logger("administrator");
---
+++ lib/executor/EEntity/EComponent/EKanopyaCollector.pm
@@ -0,0 +1,26 @@
+#    Copyright © 2011 Hedera Technology SAS
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Maintained by Dev Team of Hedera Technology <dev@hederatech.com>.
+# Created 23 april 2012
+package EEntity::EComponent::EKanopyaCollector;
+
+use strict;
+use Template;
+use String::Random;
+use base "EEntity::EComponent";
+use Log::Log4perl "get_logger";
+
+my $log = get_logger("executor");
+my $errmsg;
