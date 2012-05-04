c7a61032b5fa35cc1d86d7404254e1b44a257def
tortue <sylvain@hederatech.com>
2012-05-04 15:21:11 +0200

    include jqgrid and first use for messages

--- ui/Frontend/public/javascripts/kanopya.ui.js
+++ ui/Frontend/public/javascripts/kanopya.ui.js
@@ -1,3 +1,30 @@
 $(document).ready(function () {
-        $('body').layout({ applyDefaultStyles: true });
+    $('body').layout({ applyDefaultStyles: true });
+
+
+    $("#grid-message").jqGrid({ 
+        datatype: "local",
+        loadonce: true,
+        height: 'auto',
+        width: 'auto',
+        colNames:['Mess ID','Date', 'content'],
+        colModel:[ 
+                {name:'mess_id',index:'entity_id', width:60, sorttype:"int", hidden:true, key:true},
+                {name:'date',index:'date', width:90, sorttype:"date"},
+                {name:'content',index:'content', width:200,}],
+        //multiselect: true,
+        rowNum:5, rowList:[5,10,20,50],
+        caption: "Messages",
+        altRows: true,
+        onSelectRow: function (id) {
+            alert('Select row: ' + id);
+        },
+    });
+
+   var mydata = [ 
+                    {mess_id:"1",date:"2007-10-01",content:"salut julien"},
+                    {mess_id:"2",date:"2007-10-03",content:"bien ou bien?"},
+                ]; 
+    for(var i=0;i<=mydata.length;i++) jQuery("#grid-message").jqGrid('addRowData',i+1,mydata[i]);
+
 });
