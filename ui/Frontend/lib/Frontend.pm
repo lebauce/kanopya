58cbf658856c14d50e129cb53844ff7631d9ab83
tortue <sylvain@hederatech.com>
2012-05-10 14:34:12 +0200

    Reintroduce login/logout mecanism, session, basic style

--- ui/Frontend/lib/Frontend.pm
+++ ui/Frontend/lib/Frontend.pm
@@ -3,9 +3,23 @@ use Dancer ':syntax';
 #use Login;
 use Messager;
 
+use Login;
+
 our $VERSION = '0.1';
 
 prefix undef;
+
+Log::Log4perl->init('/opt/kanopya/conf/webui-log.conf');
+
+hook 'before' => sub {
+    $ENV{EID} = session('EID');
+};
+
+hook 'before_template' => sub {
+    my $tokens = shift;
+
+    $tokens->{username}  = session('username');
+};
 
 get '/' => sub {
     my $product = config->{kanopya_product};
---
+++ ui/Frontend/lib/Login.pm
@@ -0,0 +1,79 @@
+package Login;
+
+use Dancer ':syntax';
+use Dancer::Plugin::FormValidator;
+
+use Log::Log4perl "get_logger";
+use Administrator;
+
+my $log = get_logger('webui');
+
+get qr(/.*) => sub {
+    my $eid  = session('EID');
+    my $path = request->path;
+
+    if ( $path eq '/login' ) {
+        return pass;
+    }
+    elsif ( ! $eid  ) {
+        session login_redirect_url => $path;
+        return redirect '/login';
+    }
+    else {
+        return pass;
+    }
+};
+
+get '/login' => sub {
+    redirect '/' if ( session('EID') );
+    template 'login', {},{ layout=>'login' };
+};
+
+post '/login' => sub {
+    my $user     = param('login');
+    my $password = param('password');
+    my $redirect = session->{login_redirect_url} || '/';
+    #$redirect = '/dashboard/status' if $redirect eq '/';
+
+    my $input_hash = {
+        login    => $user,
+        password => $password
+    };
+
+    my $error = form_validator_error('login', $input_hash);
+    if ( $error ) {
+        return template 'login', { errors => $error }, { layout => 'login' };
+    }
+
+    eval {
+        Administrator::authenticate(
+            login    => $user,
+            password => $password
+        );
+    };
+
+    if ( $@ ) {
+        my $fail = {
+            user => "Authentication failed for login $user"
+        };
+        $log->error('Authentication failed for login ', $user);
+        return template 'login', { fail => $fail }, { layout => 'login' }
+    }
+    else {
+        session EID      => $ENV{EID};
+        session username => $user;
+        $log->info('Authentication succeed for login ', $user);
+        delete session->{login_redirect_url};
+        redirect $redirect;
+    }
+};
+
+get '/logout' => sub {
+    my $user = session('username');
+
+    session->destroy;
+    $log->info('Logout and session delete for login ', $user);
+    redirect '/login';
+};
+
+1;
