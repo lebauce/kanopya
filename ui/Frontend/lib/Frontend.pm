cdcf353d62816ed6ad25d2d2613e5cf63c32605e
tortue <sylvain@hederatech.com>
2012-05-10 14:34:12 +0200

    Reintroduce login/logout mecanism, session, basic style

--- ui/Frontend/lib/Frontend.pm
+++ ui/Frontend/lib/Frontend.pm
@@ -1,7 +1,21 @@
 package Frontend;
 use Dancer ':syntax';
 
+use Login;
+
 our $VERSION = '0.1';
+
+Log::Log4perl->init('/opt/kanopya/conf/webui-log.conf');
+
+hook 'before' => sub {
+    $ENV{EID} = session('EID');
+};
+
+hook 'before_template' => sub {
+    my $tokens = shift;
+
+    $tokens->{username}  = session('username');
+};
 
 get '/' => sub {
     my $product = config->{kanopya_product};
