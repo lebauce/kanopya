0f8ee472d9b13e1773344dd14b44a8ebfe02540c
Kevin Pouget <kevin.pouget@hederatech.com>
2012-05-31 15:53:25 +0200

    [core] Added classes for ServiceTemplate and Policy.

---
+++ lib/administrator/ServiceTemplate.pm
@@ -0,0 +1,69 @@
+# Copyright © 2011-2012 Hedera Technology SAS
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Maintained by Dev Team of Hedera Technology <dev@hederatech.com>.
+
+package ServiceTemplate;
+use base 'BaseDB';
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+use Log::Log4perl 'get_logger';
+
+my $log = get_logger('administrator');
+
+use constant ATTR_DEF => {
+    service_name => {
+        pattern      => '^.*$',
+        is_mandatory => 1,
+        is_extended  => 0
+    },
+    service_desc => {
+        pattern      => '^.*$',
+        is_mandatory => 0,
+        is_extended  => 0
+    },
+    hosting_policy_id => {
+        pattern      => '^\d+$',
+        is_mandatory => 1,
+        is_extended  => 0
+    },
+    storage_policy_id => {
+        pattern      => '^\d+$',
+        is_mandatory => 0,
+        is_extended  => 0
+    },
+    network_policy_id => {
+        pattern      => '^\d+$',
+        is_mandatory => 0,
+        is_extended  => 0
+    },
+    scalability_policy_id => {
+        pattern      => '^\d+$',
+        is_mandatory => 0,
+        is_extended  => 0
+    },
+    system_policy_id => {
+        pattern      => '^\d+$',
+        is_mandatory => 0,
+        is_extended  => 0
+    },
+};
+
+sub getAttrDef { return ATTR_DEF; }
+
+1;
