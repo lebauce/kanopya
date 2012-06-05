5adb92d7829509aca60c99317895f0cd4de84f8d
tortue <sylvain@hederatech.com>
2012-06-05 12:54:03 +0200

    [ui][KIO] remove unnecessary menu entry

--- ui/Frontend/public/javascripts/KIO/menu.conf.js
+++ ui/Frontend/public/javascripts/KIO/menu.conf.js
@@ -1,11 +1,5 @@
 // each link will show the div with id "view_<link_name>" and hide all div in "#view-container"
 var mainmenu_def = {
-    'Infrastructure'    : {
-        'Clusters' : [
-                     {label : 'Overview', id : 'overview'},
-                     {label : 'Hosts', id : 'hosts'}],
-        'Connectors' : [''],
-    },
     'Administration'    : {
         'Kanopya'          : [],
         'Right Management' :  [
@@ -34,16 +28,6 @@ var mainmenu_def = {
                                ]
                 }
     },
-    'Hosts'   : {
-        json : {url        : '/api/host',
-                label_key   : 'host_hostname',
-                id_key      : 'pk',
-                submenu     : [
-                               {label : 'Overview', id : 'service_overview', onLoad : displayJSON},
-                               {label : 'Hosts', id : 'service_hosts'}
-                               ]
-                }
-    }
 };
 
 var details_def = {
