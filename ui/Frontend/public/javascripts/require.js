2f4c4af0332f5fa4c6129d6aa9c7a36b396c82e8
Paul Joannon <pjoannon@hederatech.com>
2012-06-11 13:33:28 +0200

    [ui] now view SyntaxErrors when loading a script with require.js

--- ui/Frontend/public/javascripts/require.js
+++ ui/Frontend/public/javascripts/require.js
@@ -4,14 +4,18 @@ function _require() {
     var loadedScripts = {};
 
     return (function(fileName) {
+        var script;
         if (fileName in loadedScripts) { return; }
         $.ajax({
             url         : '/javascripts/' + fileName,
             type        : 'GET',
             dataType    : 'script',
             async       : false,
-            success     : function() {
+            complete    : function(jqXHR, textStatus) {
                 loadedScripts[fileName] = true;
+                if (textStatus === "parsererror") {
+                    eval(jqXHR.responseText);
+                }
             }
         });
     });
