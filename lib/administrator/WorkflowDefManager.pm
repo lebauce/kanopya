62816d6346c9cbf9b43fb164c988e4d79b6eb159
maxime <mdemoulin@hedera-technology.com>
2012-06-13 12:24:10 +0200

    [workflow] commit workflowDefManager.pm

---
+++ lib/administrator/WorkflowDefManager.pm
@@ -0,0 +1,37 @@
+#    Copyright © 2011 Hedera Technology SAS
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Maintained by Dev Team of Hedera Technology <dev@hederatech.com>.
+# Created 10 June 2012
+
+package WorkflowDefManager;
+
+use strict;
+use warnings;
+use base 'BaseDB';
+
+use constant ATTR_DEF => {
+    service_provider_manager_id              =>  {pattern       => '^.*$',
+                                 is_mandatory   => 1,
+                                 is_extended    => 0,
+                                 is_editable    => 1},
+    workflow_def_id                          =>  {pattern       => '^.*$',
+                                 is_mandatory   => 1,
+                                 is_extended    => 0,
+                                 is_editable    => 1},
+};
+
+sub getAttrDef { return ATTR_DEF; }
+
+1;
