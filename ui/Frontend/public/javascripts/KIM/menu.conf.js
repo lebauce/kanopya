e887f7f9a989f151f1d702bbf7d954f9b5e2bfb1
Paul Joannon <pjoannon@hederatech.com>
2012-06-22 17:41:51 +0200

    [ui] add right management in KIM

--- ui/Frontend/public/javascripts/KIM/menu.conf.js
+++ ui/Frontend/public/javascripts/KIM/menu.conf.js
@@ -21,7 +21,11 @@ var mainmenu_def = {
     'Services'          : getServiceMenuDefinition('cluster'),
     'Administration'    : {
         'Kanopya'          : [],
-        'Right Management' : [],
+        'Right Management' :  [
+                               {label : 'Users', id : 'users', onLoad : function(cid, eid) { require('KIO/users.js'); usersList(cid, eid); }},
+                               {label : 'Groups', id : 'groups',onLoad : function(cid, eid) { require('KIO/users.js'); groupsList(cid, eid); }},
+                               {label : 'Permissions', id : 'permissions', onLoad : function(cid, eid) { require('KIO/users.js'); permissions(cid, eid); }}
+                               ],
         'Monitoring'       : []
     },
 };
