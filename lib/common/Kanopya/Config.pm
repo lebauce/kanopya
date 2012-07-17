3e865b9c0e46904905a81e0f00e6309220220717
maxime <mdemoulin@hedera-technology.com>
2012-07-17 18:16:08 +0200

    [core][configuration] add a set function to Kanopya::Config module in order to update configuration files

--- lib/common/Kanopya/Config.pm
+++ lib/common/Kanopya/Config.pm
@@ -3,17 +3,22 @@ use strict;
 use warnings;
 use XML::Simple;
 use Storable 'dclone';
-
+use Data::Dumper;
 my $config;
 
 BEGIN {
 
     $config = {
-        executor     => XMLin('/opt/kanopya/conf/executor.conf'),
-        monitor      => XMLin('/opt/kanopya/conf/monitor.conf'), 
-        libkanopya   => XMLin('/opt/kanopya/conf/libkanopya.conf'),
-        orchestrator => XMLin('/opt/kanopya/conf/monitor.conf'), 
-        aggregator   => XMLin('/opt/kanopya/conf/aggregator.conf'), 
+        executor          => XMLin('/opt/kanopya/conf/executor.conf'),
+        executor_path     => '/opt/kanopya/conf/executor.conf',
+        monitor           => XMLin('/opt/kanopya/conf/monitor.conf'), 
+        monitor_path      => '/opt/kanopya/conf/monitor.conf',
+        libkanopya        => XMLin('/opt/kanopya/conf/libkanopya.conf'),
+        libkanopya_path   => '/opt/kanopya/conf/libkanopya.conf',
+        orchestrator      => XMLin('/opt/kanopya/conf/monitor.conf'), 
+        orchestrator_path => '/opt/kanopya/conf/monitor.conf',
+        aggregator        => XMLin('/opt/kanopya/conf/aggregator.conf.old'),
+        aggregator_path   => '/opt/kanopya/conf/aggregator.conf',
     }
 }
 
@@ -25,6 +30,17 @@ sub get {
         return $copy->{$subsystem};
     } else {
         return $copy;
+    }
+}
+
+sub set {
+    my (%args) = @_;
+    if(defined $args{config} && exists $config->{$args{subsystem}}) {
+        $config->{$args{subsystem}} = $args{config};
+        open (my $FILE, ">", $config->{"$args{subsystem}_path"}) 
+            or die 'error while loading '.qq[$config->{"$args{subsystem}_path"}: $!]; 
+        print $FILE XMLout($config->{$args{subsystem}});
+        close ($FILE);
     }
 }
 
