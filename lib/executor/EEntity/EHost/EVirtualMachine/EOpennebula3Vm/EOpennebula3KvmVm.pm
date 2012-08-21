f07b8e233037f163691b2a04b343567008b0c07d
Paul Joannon <pjoannon@hederatech.com>
2012-08-21 11:38:36 +0200

    [opennebula][kvm] add the plug/unplug cpu functions in EOpennebula3KvmVm

--- lib/executor/EEntity/EHost/EVirtualMachine/EOpennebula3Vm/EOpennebula3KvmVm.pm
+++ lib/executor/EEntity/EHost/EVirtualMachine/EOpennebula3Vm/EOpennebula3KvmVm.pm
@@ -25,4 +25,64 @@ use Log::Log4perl "get_logger";
 
 my $log = get_logger("executor");
 
+sub _onOffCpu {
+    my $self    = shift;
+    my %args    = @_;
+
+    General::checkParams(args => \%args, required => [ 'cpu', 'online' ]);
+
+    my $command = "echo $args{online} > /sys/devices/system/cpu/cpu$args{cpu}/online";
+
+    $self->getEContext->execute(command => "$command");
+}
+
+=head2 unplugCpu
+
+    Unplug a CPU by echoing 0 in his `online` file
+
+=cut
+
+sub unplugCpu {
+    my $self    = shift;
+    my %args    = @_;
+
+    General::checkParams(args => \%args, required => [ 'cpu' ]);
+
+    $self->_onOffCpu(cpu => $args{cpu}, online => 0);
+}
+
+=head2 plugCpu
+
+    Plug a CPU by echoing 1 in his `online` file
+
+=cut
+
+sub plugCpu {
+    my $self    = shift;
+    my %args    = @_;
+
+    General::checkParams(args => \%args, required => [ 'cpu' ]);
+
+    $self->_onOffCpu(cpu => $args{cpu}, online => 1);
+}
+
+=hea2 updateCpus
+
+=cut
+
+sub updateCpus {
+    my $self    = shift;
+
+    my $i       = 0;
+    while ($i < $self->opennebula3_kvm_vm_cores) {
+        if ($i < $self->host_core) {
+            $self->plugCpu(cpu => $i);
+        }
+        else {
+            $self->unplugCpu(cpu => $i);
+        }
+        ++$i;
+    }
+}
+
 1;
