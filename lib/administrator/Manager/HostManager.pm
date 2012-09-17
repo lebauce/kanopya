73eb63b447dd7b20cf8d6f07726967776eb3069d
Thomas Génin <thomas.genin@hederatech.com>
2012-09-17 11:39:09 +0200

    [overcommitment] Add generic getOvercommitmentFactors (return 1.0 for general hostmanager)

--- lib/administrator/Manager/HostManager.pm
+++ lib/administrator/Manager/HostManager.pm
@@ -195,6 +195,16 @@ sub removeHost {
     );
 }
 
+
+sub getOvercommitmentFactors {
+    my ($self) = @_;
+    return {
+        overcommitment_cpu_factor    => 1.0,
+        overcommitment_memory_factor => 1.0,
+    }
+}
+
+
 =head2 getFreeHosts
 
     Desc: return a list containing available hosts for this hosts manager
