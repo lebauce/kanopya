90b43f64407f1b5384550c15ed1d512f28cb6269
Thomas Génin <thomas.genin@hederatech.com>
2012-09-28 15:16:59 +0200

    [workflow] add new workflow_def ResubmitNode

--- scripts/database/mysql/sbin/populate_db.pl
+++ scripts/database/mysql/sbin/populate_db.pl
@@ -55,7 +55,7 @@ sub populate_workflow_def {
         }
     );
     $scale_cpu_wf->addStep( operationtype_id => $scale_op_id );
-    
+
     # ScaleIn memory workflow def
     my $scale_mem_wf = $kanopya_wf_manager->createWorkflow(
         workflow_name => 'ScaleInMemory',
@@ -73,7 +73,7 @@ sub populate_workflow_def {
         }
     );
     $scale_mem_wf->addStep( operationtype_id => $scale_op_id );
-    
+
     # AddNode workflow def
     my $addnode_op_id = Operationtype->find( hash => { operationtype_name => 'AddNode' })->id;
     my $prestart_op_id = Operationtype->find( hash => { operationtype_name => 'PreStartNode' })->id;
@@ -94,7 +94,7 @@ sub populate_workflow_def {
     $addnode_wf->addStep(operationtype_id => $prestart_op_id);
     $addnode_wf->addStep(operationtype_id => $start_op_id);
     $addnode_wf->addStep(operationtype_id => $poststart_op_id);
-    
+
     # StopNode workflow def
     my $prestop_op_id = Operationtype->find( hash => { operationtype_name => 'PreStopNode' })->id;
     my $stop_op_id = Operationtype->find( hash => { operationtype_name => 'StopNode' })->id;
@@ -113,25 +113,45 @@ sub populate_workflow_def {
     $stopnode_wf->addStep(operationtype_id => $prestop_op_id);
     $stopnode_wf->addStep(operationtype_id => $stop_op_id);
     $stopnode_wf->addStep(operationtype_id => $poststop_op_id);
-    
+
     # Optimiaas Workflow def
     my $optimiaas_wf = $kanopya_wf_manager->createWorkflow(workflow_name => 'OptimiaasWorkflow');
     my $optimiaas_op_id = Operationtype->find( hash => { operationtype_name => 'LaunchOptimiaasWorkflow' })->id;
     $optimiaas_wf->addStep(operationtype_id => $optimiaas_op_id);
-    
+
     # Migrate Workflow def
     my $migrate_wf = $kanopya_wf_manager->createWorkflow(workflow_name => 'MigrateWorkflow');
     my $migrate_op_id = Operationtype->find( hash => { operationtype_name => 'MigrateHost' })->id;
     $migrate_wf->addStep(operationtype_id => $migrate_op_id);
+
+    # ResubmitNode  workflow def
+    my $resubmit_node_wf = $kanopya_wf_manager->createWorkflow(
+        workflow_name => 'ResubmitNode',
+        params => {
+            internal => {
+                scope_id => 1,
+            },
+            automatic => {
+                context => { host => undef },
+            },
+        }
+    );
+    my $resubmit_node_op_id  = Operationtype->find( hash => { operationtype_name => 'ResubmitNode' })->id;
+    my $scale_cpu_op_id  = Operationtype->find( hash => { operationtype_name => 'ScaleCpuHost' })->id;
+    my $scale_mem_op_id  = Operationtype->find( hash => { operationtype_name => 'ScaleMemoryHost' })->id;
+    $resubmit_node_wf->addStep( operationtype_id => $resubmit_node_op_id );
+    $resubmit_node_wf->addStep( operationtype_id => $scale_cpu_op_id );
+    $resubmit_node_wf->addStep( operationtype_id => $scale_mem_op_id );
+
 }
 
 sub populate_policies {
     my %policies = ();
-    
+
     # hosting
     my $type_id = ComponentType->find(hash => { component_name => 'Physicalhoster' })->id;
     my $physicalhoster = Entity::Component->find( hash => { component_type_id => $type_id, service_provider_id => 1 } );
-    
+
     $policies{hosting} = Entity::Policy->new(
         policy_name => 'Default physical host',
         policy_desc => 'Hosting policy for default physical hosts',
@@ -139,23 +159,23 @@ sub populate_policies {
         host_manager_id     => $physicalhoster->id,
         ram      => 1024,
         ram_unit => 'M',
-        cpu      => 1, 
+        cpu      => 1,
     );
-    
+
     # storage
     my $lvm_type_id = ComponentType->find(hash => { component_name => 'Lvm' })->id;
     my $lvm = Entity::Component->find( hash => { component_type_id => $lvm_type_id, service_provider_id => 1 } );
     my $iscsit_type_id = ComponentType->find(hash => { component_name => 'Iscsitarget' })->id;
     my $iscsitarget = Entity::Component->find( hash => { component_type_id => $iscsit_type_id, service_provider_id => 1 } );
-    
+
     $policies{storage} = Entity::Policy->new(
         policy_name => 'Kanopya LVM disk exported via ISCSI',
         policy_desc => 'Datastore on Kanopya cluster for PXE boot via ISCSI',
         policy_type => 'storage',
-        disk_manager_id => $lvm->id, 
+        disk_manager_id => $lvm->id,
         export_manager_id => $iscsitarget->id,
     );
-    
+
     # network
     my $interfacerole = Entity::InterfaceRole->find(hash => {interface_role_name => 'admin'});
     my $network = Entity::Network->find(hash => {network_name => 'admin'});
@@ -168,9 +188,9 @@ sub populate_policies {
         cluster_domainname  => 'hedera-technology.com',
         interface_role_0    => $interfacerole->id,
         interface_networks_0 => $network->id,
-        default_gateway_0    => 1 
+        default_gateway_0    => 1
     );
-    
+
     # scalability
     $policies{scalability} = Entity::Policy->new(
         policy_name => 'Cluster manual scalability',
@@ -180,7 +200,7 @@ sub populate_policies {
         cluster_max_node => 10,
         cluster_priority => 1
     );
-    
+
     # system
     my $puppettypeid = ComponentType->find(hash => { component_name => 'Puppetagent' })->id;
     my $keepalivedtypeid = ComponentType->find(hash => { component_name => 'Keepalived' })->id;
@@ -196,21 +216,21 @@ sub populate_policies {
         component_type_0 => $puppettypeid,
         component_type_1 => $keepalivedtypeid,
     );
-    
+
     # billing
     $policies{billing} = Entity::Policy->new(
         policy_name => 'Empty billing configuration',
         policy_desc => 'Empty billing configuration',
         policy_type => 'billing',
     );
-    
+
     # orchestration
     $policies{orchestration} = Entity::Policy->new(
         policy_name => 'Empty orchestration configuration',
         policy_desc => 'Empty orchestration configuration',
         policy_type => 'orchestration',
     );
-    
+
     return \%policies;
 }
 
