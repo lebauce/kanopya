a4b3b159a1decb9931986b5a7ea6ec18918b9bdb
Thomas MANNI <thomas.manni@hederatech.com>
2012-10-19 16:00:44 +0200

    [doc] pod documentation added to Kanopya::Config

--- lib/common/Kanopya/Config.pm
+++ lib/common/Kanopya/Config.pm
@@ -1,3 +1,30 @@
+#    Copyright © 2012 Hedera Technology SAS
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=pod
+
+=begin classdoc
+
+Subroutines to read/write kanopya configuration files.
+
+@since    2012-May-21
+
+=end classdoc
+
+=cut
+
 package Kanopya::Config;
 use strict;
 use warnings;
@@ -7,6 +34,16 @@ use Path::Class;
 
 my $config;
 my $kanopya_dir;
+
+=pod
+
+=begin classdoc
+
+Initialize array reference $config from kanopya xml configuration files
+
+=end classdoc
+
+=cut
 
 sub _loadconfig {
     #get Kanopya base directory
@@ -28,12 +65,38 @@ sub _loadconfig {
     }
 }
 
+=pod
+
+=begin classdoc
+
+retrieve kanopya directory 
+
+@return kanopya directory 
+
+=end classdoc
+
+=cut
+
 sub getKanopyaDir {
     if(not defined $kanopya_dir) {
         _loadconfig();
     }
     return $kanopya_dir;
 }
+
+=pod
+
+=begin classdoc
+
+get the whole configuration or a specific subsystem configuration 
+
+@optional $subsystem : a string among 'executor','monitor','libkanopya','orchestrator','aggregator'
+
+@return hash reference containing the configuration
+
+=end classdoc
+
+=cut
 
 sub get {
     my ($subsystem) = @_;
@@ -53,6 +116,16 @@ sub get {
         return $copy;
     }
 }
+
+=pod
+
+=begin classdoc
+
+TODO
+
+=end classdoc
+
+=cut
 
 sub set {
     my (%args) = @_;
