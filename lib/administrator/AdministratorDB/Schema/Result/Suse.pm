f7528447f4bbdd4042be490b2bdc4debf0e4a2ac
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2012-11-02 15:14:46 +0100

    [core] Regenerate DBIx class for Suse component

--- lib/administrator/AdministratorDB/Schema/Result/Suse.pm
+++ lib/administrator/AdministratorDB/Schema/Result/Suse.pm
@@ -75,6 +75,11 @@ __PACKAGE__->belongs_to(
 # Created by DBIx::Class::Schema::Loader v0.07024 @ 2012-10-29 20:28:20
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:uok08dSlewNnt5s0svHT3A
 
+__PACKAGE__->belongs_to(
+  "parent",
+  "AdministratorDB::Schema::Result::Linux",
+  { "foreign.linux_id" => "self.suse_id" },
+  { cascade_copy => 0, cascade_delete => 1 } 
+);
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
