1ee913992a667ba6d7241edb9b7e759ffdf77b32
Kevin Pouget <kevin.pouget@hederatech.com>
2012-11-28 12:19:24 +0100

    [ui] Fix iscsitarget configuration.

--- ui/Frontend/public/javascripts/KIM/component.js
+++ ui/Frontend/public/javascripts/KIM/component.js
@@ -8,6 +8,11 @@ var Component = (function() {
             var componentType = fromIdToComponentType(ajax('GET', '/api/component/' + this.id).component_type_id);
             this.type = componentType.component_name.toLowerCase() + componentType.component_version;
             this.name = componentType.component_name;
+
+            // Work around to handle components without version number
+            if (window[this.type.ucfirst()] == undefined) {
+                this.type = this.name.toLowerCase();
+            }
         }
 
         this.displayed = [];
