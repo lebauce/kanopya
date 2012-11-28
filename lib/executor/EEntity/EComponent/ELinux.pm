8598631b3fedbb4d331c5ab3545efe9f6d0e2f32
Thomas MANNI <thomas.manni@hederatech.com>
2012-11-28 10:52:32 +0100

    [core] sles initrd generation for iscsi and multipath

--- lib/executor/EEntity/EComponent/ELinux.pm
+++ lib/executor/EEntity/EComponent/ELinux.pm
@@ -1,16 +1,31 @@
-#    Copyright © 2011 Hedera Technology SAS
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+# Copyright © 2011 Hedera Technology SAS
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=pod
+
+=begin classdoc
+
+Linux component base class to generate system configuration files.
+
+@since    2012-Jun-10
+@instance hash
+@self     $self
+
+=end classdoc
+
+=cut
+
 package EEntity::EComponent::ELinux;
 use base 'EEntity::EComponent';
 
@@ -124,6 +139,20 @@ sub preconfigureSystemimage {
 }
 
 # individual file generation
+
+=pod
+
+=begin classdoc
+
+Generate the hostname configuration file
+
+@param host Entity::Host instance
+@param cluster Entity::ServiceProvider::Inside::Cluster instance
+@return hashref with src as full path of the generated file, dest as the full path destination
+
+=end classdoc
+
+=cut
 
 sub _generateHostname {
     my ($self, %args) = @_;
@@ -526,5 +555,7 @@ sub _writeNetConf {
 
     $log->info("Skipping configuration of network for cluster " . $args{cluster}->cluster_name);
 }
+
+
 
 1;
