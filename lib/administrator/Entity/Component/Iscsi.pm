1aa03306f09e9f0a46d7b08e6fcdb03a55c14b8f
Kevin Pouget <kevin.pouget@hederatech.com>
2012-11-28 12:09:49 +0100

    [core] Added administrator side classes for Iscsi, IscsiPortal and Storage.

---
+++ lib/administrator/Entity/Component/Iscsi.pm
@@ -0,0 +1,163 @@
+# Copyright © 2012 Hedera Technology SAS
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Maintained by Dev Team of Hedera Technology <dev@hederatech.com>.
+
+package Entity::Component::Iscsi;
+use base "Entity::Component";
+use base "Manager::ExportManager";
+
+use strict;
+use warnings;
+
+use Kanopya::Exceptions;
+use Entity::Component::Iscsi::IscsiPortal;
+
+use Log::Log4perl "get_logger";
+use Data::Dumper;
+
+my $log = get_logger("");
+my $errmsg;
+
+use constant ATTR_DEF => {
+    iscsi_portals => {
+        label        => 'ISCSI portals',
+        type         => 'relation',
+        relation     => 'single_multi',
+        is_mandatory => 0,
+        is_editable  => 1,
+    },
+    export_type => {
+        is_virtual => 1
+    }
+};
+
+sub getAttrDef { return ATTR_DEF; }
+
+sub methods {}
+
+sub exportType {
+    return "Unmanaged ISCSI target";
+}
+
+sub checkExportManagerParams {
+    my $self = shift;
+    my %args = @_;
+    
+    General::checkParams(args => \%args, required => [ "iscsi_portals", "target", "lun" ]);
+}
+
+sub getPolicyParams {
+    my $self = shift;
+    my %args = @_;
+
+    General::checkParams(args => \%args, required => [ 'policy_type' ]);
+
+    my $portals = {};
+    if ($args{policy_type} eq 'storage') {
+        for my $portal (@{ $self->getConf->{iscsi_portals} }) {
+            $portals->{$portal->{iscsi_portal_id}} = $portal->{iscsi_portal_ip} . ':' . $portal->{iscsi_portal_port}
+        }
+        return [ { name => 'iscsi_portals', label => 'ISCSI portals', values => $portals },
+                 { name => 'target', label => 'ISCSI target' },
+                 { name => 'lun', label => 'LUN number' } ];
+    }
+    return [];
+}
+
+sub getConf {
+    my $self = shift;
+    my $conf = {};
+    my @portals = ();
+
+    for my $portal ($self->iscsi_portals) {
+        push @portals, { iscsi_portal_ip   => $portal->iscsi_portal_ip,
+                         iscsi_portal_port => $portal->iscsi_portal_port,
+                         iscsi_portal_id   => $portal->iscsi_portal_id,
+                         iscsi_id          => $self->id };
+    }
+
+    $conf->{iscsi_portals} = \@portals;
+    return $conf;
+}
+
+sub setConf {
+    my $self = shift;
+    my %args = @_;
+
+    General::checkParams(args => \%args, required => [ 'conf' ]);
+
+    my $conf = $args{conf};
+    my $configured = {};
+    for my $portal (@{ $conf->{iscsi_portals} }) {
+        my $portal_id = $portal->{iscsi_portal_id};
+        if (not $portal_id) {
+            $portal_id = Entity::Component::Iscsi::IscsiPortal->new(
+                             iscsi_id          => $self->id,
+                             iscsi_portal_ip   => $portal->{iscsi_portal_ip},
+                             iscsi_portal_port => $portal->{iscsi_portal_port},
+                         )->id;
+        }
+        $configured->{$portal_id} = 1;
+    }
+    for my $existing ($self->iscsi_portals) {
+        if (not defined $configured->{$existing->id}) {
+            $existing->remove();
+        }
+    }
+}
+
+sub getExportManagerFromBootPolicy {
+    my $self = shift;
+    my %args = @_;
+
+    General::checkParams(args => \%args, required => [ "boot_policy" ]);
+
+    throw Kanopya::Exception::Internal::UnknownCategory(
+              error => "Unsupported boot policy: $args{boot_policy}"
+          );
+}
+
+sub getBootPolicyFromExportManager {
+    my $self = shift;
+    my %args = @_;
+
+    General::checkParams(args => \%args, required => [ "export_manager" ]);
+
+    throw Kanopya::Exception::Internal::UnknownCategory(
+              error => "Unsupported export manager:" . $args{export_manager}
+          );
+}
+
+# Insert default configuration in db for this component 
+sub insertDefaultConfiguration {
+    my $self = shift;
+
+    my $ip = $self->service_provider->getMasterNodeIp;
+    if (not $ip) {
+        throw Kanopya::Exception::Internal(
+                  error => "Could not find master not ip while " .
+                           "inserting default configuration"
+              );
+    }
+
+    Entity::Component::Iscsi::IscsiPortal->new(
+        iscsi_id          => $self->id,
+        iscsi_portal_ip   => $ip,
+        iscsi_portal_port => 3261
+    );
+}
+
+1;
---
+++ lib/administrator/Entity/Component/Iscsi/Iscsitarget1.pm
@@ -0,0 +1,256 @@
+#    Copyright 2011 Hedera Technology SAS
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Maintained by Dev Team of Hedera Technology <dev@hederatech.com>.
+
+package Entity::Component::Iscsi::Iscsitarget1;
+use base "Entity::Component::Iscsi";
+use base "Manager::ExportManager";
+
+use strict;
+use warnings;
+
+use General;
+use Administrator;
+use Kanopya::Exceptions;
+
+use Entity::Operation;
+use Entity::Container;
+use Entity::ContainerAccess::IscsiContainerAccess;
+
+use Log::Log4perl "get_logger";
+use Data::Dumper;
+
+
+my $log = get_logger("");
+my $errmsg;
+
+use constant ATTR_DEF => {
+    export_type => {
+        is_virtual => 1
+    }
+};
+
+sub getAttrDef { return ATTR_DEF; }
+
+sub exportType {
+    return "ISCSI target";
+}
+
+use constant ACCESS_MODE => {
+    READ_WRITE => 'wb',
+    READ_ONLY  => 'ro',
+};
+
+sub checkExportManagerParams {
+    my $self = shift;
+    my %args = @_;
+    
+    General::checkParams(args => \%args, required => [ "iscsi_portals" ]);
+}
+
+sub getPolicyParams {
+    my $self = shift;
+    my %args = @_;
+
+    General::checkParams(args => \%args, required => [ 'policy_type' ]);
+
+    my $portals = {};
+    if ($args{policy_type} eq 'storage') {
+        for my $portal (@{ $self->SUPER::getConf->{iscsi_portals} }) {
+            $portals->{$portal->{iscsi_portal_id}} = $portal->{iscsi_portal_ip} . ':' . $portal->{iscsi_portal_port}
+        }
+        return [ { name => 'iscsi_portals', label => 'ISCSI portals', values => $portals } ];
+    }
+    return [];
+}
+
+sub getConf {
+    my $self = shift;
+    my @targets = ();
+
+    my $conf = $self->SUPER::getConf();
+
+    my @accesses = Entity::ContainerAccess->search(
+                       hash => { export_manager_id => $self->getAttr(name => 'entity_id') }
+                   );
+
+    for my $access (@accesses) {
+        my @luns = ();
+        push @luns, {
+            iscsitarget1_lun_id     => $access->getContainer->id,
+            iscsitarget1_lun_number => $access->getAttr(name => 'lun_name'),
+            iscsitarget1_lun_device => $access->getContainer->getAttr(name => 'container_device'),
+            iscsitarget1_lun_typeio => $access->getAttr(name => 'typeio'),
+            iscsitarget1_lun_iomode => $access->getAttr(name => 'iomode'),
+        };
+        push @targets, {
+            iscsitarget1_target_name => $access->getAttr(name => 'container_access_export'),
+            iscsitarget1_target_id   => $access->getAttr(name => 'entity_id'),
+            luns => \@luns
+        };
+    }
+
+    $conf->{targets} = \@targets;
+    return $conf;
+}
+
+sub setConf {
+    my $self = shift;
+    my %args = @_;
+
+    General::checkParams(args => \%args, required => ['conf']);
+
+    $self->SUPER::setConf(%args);
+
+    my $conf = $args{conf};
+    for my $target ( @{ $conf->{targets} } ) {
+        LUN:
+        for my $lun ( @{ $target->{luns} } ) {
+            # Create the export if not already exists
+            if (not $lun->{iscsitarget1_lun_id}) {
+                my @containers = Entity::Container->search(hash => {});
+
+                # Check if specified device match to a registred container.
+                my $container;
+                foreach my $cont (@containers) {
+                    my $device = $cont->getAttr(name => 'container_device');
+                    if ("$device" eq "$lun->{iscsitarget1_lun_device}") {
+                        $container = $cont;
+                        last;
+                    }
+                }
+                if (! defined $container) {
+                    $errmsg = "Specified device <$lun->{iscsitarget1_lun_device}> " .
+                              "does not match to an existing container.";
+                    $log->error($errmsg);
+                    throw Kanopya::Exception::Internal::WrongValue(error => $errmsg);
+                }
+
+                $self->createExport(container   => $container,
+                                    export_name => $container->container_name,
+                                    typeio      => $lun->{iscsitarget1_lun_typeio},
+                                    iomode      => $lun->{iscsitarget1_lun_iomode});
+            }
+
+            # Temporary: we can create only one lun with one target
+            last LUN;
+        }        
+    }
+}
+
+# return a data structure to pass to the template processor 
+sub getTemplateData {
+    my $self = shift;
+    my $targets = { };
+    my @results = ();
+
+    my @exports = Entity::ContainerAccess->search(
+                      hash => { export_manager_id => $self->getAttr(name => "component_id") }
+                  );
+
+    for my $export (@exports) {
+        my $target;
+        my $target_name = $export->getAttr(name => "container_access_export");
+        my $luns;
+
+        if (defined $targets->{$target_name}) {
+            $target = $targets->{$target_name};
+        }
+        else {
+            $target = { luns        => [],
+                        target_name => $target_name };
+        }
+
+        my $container = Entity::Container->get(
+                            id => $export->getAttr(name => 'container_id')
+                        );
+
+        push @{$target->{luns}}, {
+            number => 0, # $export->getAttr(name => 'number'),
+            device => $container->getAttr(name => 'container_device'),
+            typeio => $export->getAttr(name => 'typeio'),
+            iomode => $export->getAttr(name => 'iomode'),
+        };
+
+        $targets->{$target_name} = $target;
+    }
+
+    my @values = values %{$targets};
+    return { targets => \@values };
+}
+
+=head2 getNetConf
+B<Class>   : Public
+B<Desc>    : This method return component network configuration in a hash ref, it's indexed by port and value is the port
+B<args>    : None
+B<Return>  : hash ref containing network configuration with following format : {port => protocol}
+B<Comment>  : None
+B<throws>  : Nothing
+=cut
+
+sub getNetConf {
+    return { 3260 => ['tcp'] };
+}
+
+sub getReadOnlyParameter {
+    my $self = shift;
+    my %args = @_;
+
+    General::checkParams(args => \%args, required => [ 'readonly' ]);
+
+    my $value;
+    if ($args{readonly}) { $value = 'ro'; }
+    else                 { $value = 'wb'; }
+    return {
+        name  => 'iomode',
+        value => $value,
+    }
+}
+
+=head2 createExport
+
+    Desc : Implement createExport from ExportManager interface.
+           This function enqueue a ECreateExport operation.
+    args : export_name, device, typeio, iomode
+
+=cut
+
+sub createExport {
+    my $self = shift;
+    my %args = @_;
+
+    General::checkParams(args     => \%args,
+                         required => [ "container", "export_name", "typeio", "iomode" ]);
+
+    $log->debug("New Operation CreateExport with attrs : " . %args);
+    Entity::Operation->enqueue(
+        priority => 200,
+        type     => 'CreateExport',
+        params   => {
+            context => {
+                export_manager => $self,
+                container      => $args{container},
+            },
+            manager_params => {
+                export_name => $args{export_name},
+                typeio      => $args{typeio},
+                iomode      => $args{iomode},
+            },
+        },
+    );
+}
+
+1;
