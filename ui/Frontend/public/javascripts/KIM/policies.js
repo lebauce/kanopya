9c09b22540364893ac29c5ef37ff3183533d1cfe
Kevin Pouget <kevin.pouget@hederatech.com>
2012-12-07 17:56:52 +0100

    [ui] Improve the experimental mode for policies.

--- ui/Frontend/public/javascripts/KIM/policies.js
+++ ui/Frontend/public/javascripts/KIM/policies.js
@@ -36,10 +36,18 @@ function load_policy_content (container_id) {
         buttonexp.bind('click', function() {
             // Use the kanopyaformwizard for policies (experimental)
             (new KanopyaFormWizard({
-                title          : 'Add a ' + policy_type + ' policy (experimental)',
-                type           : 'policy',
-                displayed      : [ 'policy_name', 'policy_desc' ]
-        })).start();
+                title : 'Add a ' + policy_type + ' policy (experimental)',
+                type  : policy_type + 'policy',
+                rawattrdef : {
+                    policy_type : {
+                        value : policy_type
+                    }
+                },
+                attrsCallback : function (resource, data) {
+                    return ajax('POST', '/api/' + policy_type + 'policy/getPolicyDef', data);
+                },
+                callback : function () { grid.trigger("reloadGrid"); }
+            })).start();
         });
 
         $('#' + cid).append(button);
@@ -47,7 +55,7 @@ function load_policy_content (container_id) {
     };
 
     var container = $('#' + container_id);
-    var grid = create_grid( {
+    var grid = create_grid({
         url: '/api/policy?policy_type=' + policy_type,
         content_container_id: container_id,
         grid_id: policy_type + '_policy_list',
@@ -56,7 +64,7 @@ function load_policy_content (container_id) {
                     { name:'policy_name', index:'policy_name', width:300 },
                     { name:'policy_desc', index:'policy_desc', width:500 } ],
         details: { onSelectRow : load_policy_details }
-    } );
+    });
 
     createAddPolicyButton(container_id, grid);
 }
@@ -72,39 +80,59 @@ function load_policy_details (elem_id, row_data, grid_id) {
             policy = d;
         }, this)
     });
+//
+//    var flattened_policy;
+//    $.ajax({
+//        type     : 'POST',
+//        async    : false,
+//        url      : '/api/policy/' + elem_id + '/getFlattenedHash',
+//        dataTYpe : 'json',
+//        success  : $.proxy(function(d) {
+//            flattened_policy = d;
+//        }, this)
+//    });
+//
+//    jQuery.extend(true, flattened_policy, policy);
+//
+//    var fields = jQuery.extend(true, {}, policies[policy.policy_type]);
+//    fields['policy_id'] = {
+//        label        : 'Policy id',
+//        type         : 'hidden',
+//        value        : policy.policy_id,
+//    };
 
-    var flattened_policy;
-    $.ajax({
-        type     : 'POST',
-        async    : false,
-        url      : '/api/policy/' + elem_id + '/getFlattenedHash',
-        dataTYpe : 'json',
-        success  : $.proxy(function(d) {
-            flattened_policy = d;
-        }, this)
-    });
-
-    jQuery.extend(true, flattened_policy, policy);
-
-    var fields = jQuery.extend(true, {}, policies[policy.policy_type]);
-    fields['policy_id'] = {
-        label        : 'Policy id',
-        type         : 'hidden',
-        value        : policy.policy_id,
-    };
-
-    var policy_opts = {
-        title       : 'Edit the ' + policy.policy_type + ' policy: ' + policy.policy_name,
-        name        : 'policy',
-        fields      : fields,
-        values      : flattened_policy,
-        callback    : function () { $('#' + grid_id).trigger("reloadGrid"); }
-    };
+//    var policy_opts = {
+//        title       : 'Edit the ' + policy.policy_type + ' policy: ' + policy.policy_name,
+//        name        : 'policy',
+//        fields      : fields,
+//        values      : flattened_policy,
+//        callback    : function () { $('#' + grid_id).trigger("reloadGrid"); }
+//    };
 
     // Special management for Orchestration policy
     if (policy.policy_type == 'orchestration') {
         load_orchestration_policy_details(policy_opts, flattened_policy, grid_id);
+
     } else {
-        new PolicyForm(policy_opts).start();
+//        new PolicyForm(policy_opts).start();
+
+        // Use the kanopyaformwizard for policies (experimental)
+        (new KanopyaFormWizard({
+            title : 'Edit the ' + policy.policy_type + ' policy: ' + policy.policy_name,
+            type  : 'policy',
+            id    : elem_id,
+            rawattrdef : {
+                policy_type : {
+                    value : policy.policy_type
+                }
+            },
+            attrsCallback : function (resource, data) {
+                return ajax('POST', '/api/' + policy.policy_type + 'policy/' + policy.pk + '/getPolicyDef', data);
+            },
+            valuesCallback : function (type, id, attributes) {
+                return ajax('GET', '/api/' + policy.policy_type + 'policy/' + policy.pk);
+            },
+            callback : function () { $('#' + grid_id).trigger("reloadGrid"); }
+        })).start();
     }
 }
