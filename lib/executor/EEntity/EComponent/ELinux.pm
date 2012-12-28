0d1d18c873488999c25b12ed94536781c7532eb8
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2012-12-28 18:00:18 +0100

    [debian] Move Debian specific initrd generation code to the component

--- lib/executor/EEntity/EComponent/ELinux.pm
+++ lib/executor/EEntity/EComponent/ELinux.pm
@@ -640,11 +640,7 @@ sub buildInitramfs {
     my $compress = $args{compress_type};
     
     # rebuild and compress the new initrd
-    my $kanopya_dir = Kanopya::Config::getKanopyaDir();
-    my $cmd = "cp -R $kanopya_dir/tools/deployment/system/initramfs-tools/scripts/* $initrddir/scripts";
-    $econtext->execute(command => $cmd);
-
-    $cmd = "(cd $initrddir && find . | cpio -H newc -o | $compress > $newinitrd)";
+    my $cmd = "(cd $initrddir && find . | cpio -H newc -o | $compress > $newinitrd)";
     $econtext->execute(command => $cmd);
 
     # finaly we remove the temporary directory
