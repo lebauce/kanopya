b23476ab6b1b124e4009efe4744f528550183413
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2013-01-29 10:14:11 +0100

    [php] Install required php modules along php

--- lib/administrator/Entity/Component/Php5.pm
+++ lib/administrator/Entity/Component/Php5.pm
@@ -16,41 +16,6 @@
 # Maintained by Dev Team of Hedera Technology <dev@hederatech.com>.
 # Created 4 sept 2010
 
-=head1 NAME
-
-<Entity::Component::Php5> <Php5 component concret class>
-
-=head1 VERSION
-
-This documentation refers to <Entity::Component::Php5> version 1.0.0.
-
-=head1 SYNOPSIS
-
-use <Entity::Component::Php5>;
-
-my $component_instance_id = 2; # component instance id
-
-Entity::Component::Php5->get(id=>$component_instance_id);
-
-# Cluster id
-
-my $cluster_id = 3;
-
-# Component id are fixed, please refer to component id table
-
-my $component_id =2 
-
-Entity::Component::Php5->new(component_id=>$component_id, cluster_id=>$cluster_id);
-
-=head1 DESCRIPTION
-
-Entity::Component::Php5 is class allowing to instantiate a Php5 component
-This Entity is empty but present methods to set configuration.
-
-=head1 METHODS
-
-=cut
-
 package Entity::Component::Php5;
 use base "Entity::Component";
 
@@ -125,66 +90,8 @@ sub getBaseConfiguration {
 sub getPuppetDefinition {
     my ($self, %args) = @_;
 
-    return "class { 'apache::mod::php': }\n";
+    return "class { 'apache::mod::php': }\n" .
+           "class { 'php5': }\n";
 }
-
-=head1 DIAGNOSTICS
-
-Exceptions are thrown when mandatory arguments are missing.
-Exception : Kanopya::Exception::Internal::IncorrectParam
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-This module need to be used into Kanopya environment. (see Kanopya presentation)
-This module is a part of Administrator package so refers to Administrator configuration
-
-=head1 DEPENDENCIES
-
-This module depends of 
-
-=over
-
-=item KanopyaException module used to throw exceptions managed by handling programs
-
-=item Entity::Component module which is its mother class implementing global component method
-
-=back
-
-=head1 INCOMPATIBILITIES
-
-None
-
-=head1 BUGS AND LIMITATIONS
-
-There are no known bugs in this module.
-
-Please report problems to <Maintainer name(s)> (<contact address>)
-
-Patches are welcome.
-
-=head1 AUTHOR
-
-<HederaTech Dev Team> (<dev@hederatech.com>)
-
-=head1 LICENCE AND COPYRIGHT
-
-Kanopya Copyright (C) 2009, 2010, 2011, 2012, 2013 Hedera Technology.
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3, or (at your option)
-any later version.
-
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; see the file COPYING.  If not, write to the
-Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-Boston, MA 02110-1301 USA.
-
-=cut
 
 1;
