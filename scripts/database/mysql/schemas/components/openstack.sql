8db4b2d505866963af25740e6bb60a220dbc455e
Paul Joannon <paul.joannon@gmail.com>
2013-02-04 11:34:48 +0100

    [openstack] fix syntax errors in openstack.sql

--- scripts/database/mysql/schemas/components/openstack.sql
+++ scripts/database/mysql/schemas/components/openstack.sql
@@ -7,7 +7,7 @@ SET foreign_key_checks=0;
 --
 
 CREATE TABLE `glance` (
-  `glance_id` int(8) unsigned NOT NULL
+  `glance_id` int(8) unsigned NOT NULL,
   PRIMARY KEY (`glance_id`),
   CONSTRAINT `fk_glance_1` FOREIGN KEY (`glance_id`) REFERENCES `component` (`component_id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -17,7 +17,7 @@ CREATE TABLE `glance` (
 --
 
 CREATE TABLE `keystone` (
-  `keystone_id` int(8) unsigned NOT NULL
+  `keystone_id` int(8) unsigned NOT NULL,
   PRIMARY KEY (`keystone_id`),
   CONSTRAINT `fk_keystone_1` FOREIGN KEY (`keystone_id`) REFERENCES `component` (`component_id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -27,7 +27,7 @@ CREATE TABLE `keystone` (
 --
 
 CREATE TABLE `nova_compute` (
-  `nova_compute_id` int(8) unsigned NOT NULL
+  `nova_compute_id` int(8) unsigned NOT NULL,
   PRIMARY KEY (`nova_compute_id`),
   CONSTRAINT `fk_nova_compute_1` FOREIGN KEY (`nova_compute_id`) REFERENCES `component` (`component_id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -37,7 +37,7 @@ CREATE TABLE `nova_compute` (
 --
 
 CREATE TABLE `nova_controller` (
-  `nova_controller_id` int(8) unsigned NOT NULL
+  `nova_controller_id` int(8) unsigned NOT NULL,
   PRIMARY KEY (`nova_controller_id`),
   CONSTRAINT `fk_nova_controller_1` FOREIGN KEY (`nova_controller_id`) REFERENCES `component` (`component_id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -47,7 +47,9 @@ CREATE TABLE `nova_controller` (
 --
 
 CREATE TABLE `quantum` (
-  `quantum_id` int(8) unsigned NOT NULL
+  `quantum_id` int(8) unsigned NOT NULL,
   PRIMARY KEY (`quantum_id`),
   CONSTRAINT `fk_quantum_1` FOREIGN KEY (`quantum_id`) REFERENCES `component` (`component_id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET foreign_key_checks=1;
