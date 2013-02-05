f5f6b7e8c9408fed547636b485caebd18612bd6f
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2013-02-05 16:56:11 +0100

    [linux] Disable resolvconf service

--- templates/components/puppetmaster/modules/linux/manifests/init.pp
+++ templates/components/puppetmaster/modules/linux/manifests/init.pp
@@ -1,8 +1,15 @@
 class linux ($sourcepath) {
-      file {'/etc/hosts':
-      	   path    => '/etc/hosts',
-	   ensure  => present,
-	   mode    => 0644,
-	   source  => "puppet:///kanopyafiles/${sourcepath}/etc/hosts",
-	   }
+	file { '/etc/hosts':
+		path    => '/etc/hosts',
+		ensure  => present,
+		mode    => 0644,
+		source  => "puppet:///kanopyafiles/${sourcepath}/etc/hosts",
+	}
+	service { 'resolvconf':
+		name       => 'resolvconf',
+		ensure     => stopped,
+		hasstatus  => true,
+		hasrestart => true,
+		enable     => false,
+	}
 }  
