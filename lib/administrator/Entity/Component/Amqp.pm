80b3906009dd511beacecac8b8b5152b7b5afe2f
Paul Joannon <paul.joannon@gmail.com>
2013-02-05 15:18:32 +0100

    [amqp] write getPuppetDefinition function

--- lib/administrator/Entity/Component/Amqp.pm
+++ lib/administrator/Entity/Component/Amqp.pm
@@ -25,4 +25,17 @@ use constant ATTR_DEF => { };
 
 sub getAttrDef { return ATTR_DEF };
 
+sub getPuppetDefinition {
+    my ($self, %args) = @_;
+
+    my $definitions = "\$rabbitmq_repo = \$operatingsystem ? {\n" .
+                      "\t/(?i)(debian|ubuntu)/ => 'rabbitmq::repo::apt'\n" .
+                      "\tdefault => 'rabbitmq::repo::rhel'\n" .
+                      "}\n" .
+                      "class { \"\$rabbitmq_repo\": }\n" .
+                      "class { 'rabbitmq::server': }\n";
+
+    return $definitions;
+}
+
 1;
