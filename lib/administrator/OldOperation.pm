1f14a877228878173cfd491d8bc4dff0d1c8edb7
Kevin Pouget <kevin.pouget@hederatech.com>
2013-02-11 14:49:06 +0100

    [core] Add classes for OldOperation and OldOperationParameter.

---
+++ lib/administrator/OldOperation.pm
@@ -0,0 +1,79 @@
+# Copyright © 2011-2012 Hedera Technology SAS
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Maintained by Dev Team of Hedera Technology <dev@hederatech.com>.
+
+package OldOperation;
+use base 'BaseDB';
+
+use strict;
+use warnings;
+
+use Log::Log4perl 'get_logger';
+
+my $log = get_logger("");
+
+use constant ATTR_DEF => {
+    type => {
+        pattern      => '^.*$',
+        is_mandatory => 0,
+        is_extended  => 0
+    },
+    workflow_id => {
+        pattern      => '^\d+$',
+        is_mandatory => 0,
+        is_extended  => 0
+    },
+    user_id => {
+        pattern      => '^\d+$',
+        is_mandatory => 0,
+        is_extended  => 0
+    },
+    priority => {
+        pattern      => '^\d+$',
+        is_mandatory => 0,
+        is_extended  => 0
+    },
+    creation_date => {
+        pattern      => '^.*$',
+        is_mandatory => 0,
+        is_extended  => 0
+    },
+    creation_time => {
+        pattern      => '^.*$',
+        is_mandatory => 0,
+        is_extended  => 0
+    },
+    execution_date => {
+        pattern      => '^.*$',
+        is_mandatory => 0,
+        is_extended  => 0
+    },
+    execution_time => {
+        pattern      => '^.*$',
+        is_mandatory => 0,
+        is_extended  => 0
+    },
+    execution_status => {
+        pattern      => '^ready|processing|prereported|postreported|waiting_validation|' .
+                        'validated|blocked|cancelled|succeeded|pending$',
+        is_mandatory => 0,
+        is_extended  => 0
+    },
+};
+
+sub getAttrDef { return ATTR_DEF; }
+
+1;
