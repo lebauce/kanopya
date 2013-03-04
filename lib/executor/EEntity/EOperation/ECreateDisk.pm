72230de3e12acd2716ba7f304970462e8f110b3d
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2013-03-04 12:34:30 +0100

    [core] Fix disk creation and export operations

--- lib/executor/EEntity/EOperation/ECreateDisk.pm
+++ lib/executor/EEntity/EOperation/ECreateDisk.pm
@@ -63,7 +63,7 @@ sub prepare {
                          required => [ "name", "size", "filesystem" ]);
 
     # Check service provider state
-    my $storage_provider = $self->{context}->{disk_manager}->disk_manager->service_provider;
+    my $storage_provider = $self->{context}->{disk_manager}->service_provider;
     my ($state, $timestamp) = $storage_provider->getState();
     if ($state ne 'up'){
         $errmsg = "Service provider has to be up !";
@@ -85,64 +85,5 @@ sub execute {
 
     $log->info("New container <" . $container->container_name . "> created");
 }
-
-=head1 DIAGNOSTICS
-
-Exceptions are thrown when mandatory arguments are missing.
-Exception : Kanopya::Exception::Internal::IncorrectParam
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-This module need to be used into Kanopya environment. (see Kanopya presentation)
-This module is a part of Administrator package so refers to Administrator configuration
-
-=head1 DEPENDENCIES
-
-This module depends of 
-
-=over
-
-=item KanopyaException module used to throw exceptions managed by handling programs
-
-=item Entity::Component module which is its mother class implementing global component method
-
-=back
-
-=head1 INCOMPATIBILITIES
-
-None
-
-=head1 BUGS AND LIMITATIONS
-
-There are no known bugs in this module.
-
-Please report problems to <Maintainer name(s)> (<contact address>)
-
-Patches are welcome.
-
-=head1 AUTHOR
-
-<HederaTech Dev Team> (<dev@hederatech.com>)
-
-=head1 LICENCE AND COPYRIGHT
-
-Kanopya Copyright (C) 2009, 2010, 2011, 2012, 2013 Hedera Technology.
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3, or (at your option)
-any later version.
-
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; see the file COPYING.  If not, write to the
-Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-Boston, MA 02110-1301 USA.
-
-=cut
 
 1;
