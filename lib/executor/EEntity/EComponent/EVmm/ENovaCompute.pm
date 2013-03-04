e6e292e87a51c1a3ea5453169ef88a323237f1bb
Maxime <maxime.demoulin@hedera-technology.com>
2013-03-04 14:22:47 +0100

    [openstack] Basic implementation of VMM interface on NovaCompute

--- lib/executor/EEntity/EComponent/EVmm/ENovaCompute.pm
+++ lib/executor/EEntity/EComponent/EVmm/ENovaCompute.pm
@@ -19,6 +19,7 @@ use strict;
 use warnings;
 
 use EEntity;
+use OpenStack::API;
 
 sub postStartNode {
     my ($self, %args) = @_;
@@ -43,5 +44,70 @@ sub postStartNode {
     $self->SUPER::postStartNode(%args);
 }
 
-1;
+sub getAvailableMemory {
+    my ($self, %args) = @_;
+ 
+    General::checkParams(args => \%args, required => [ "host" ],);
 
+    my $host = $args{host};
+    my $hostname = $host->node->node_hostname;
+
+    my $host_details = $self->api->tenant(id => $self->api->{tenant_id} . '/os-hypervisors/detail')
+                           ->get(target => 'compute')->{hypervisors};
+
+    my ($hypervisor) = grep { $_->{hypervisor_hostname} eq $host->fqdn } @$host_details;
+
+    my $memory = {
+        mem_effectively_available   => $hypervisor->{free_ram_mb} * 1025 * 1024,
+        mem_theoretically_available => ($host->host_ram * $self->overcommitment_memory_factor) -
+                                       ($hypervisor->{memory_mb_used} * 1024 * 1024),
+    };
+
+    return $memory;
+}
+
+sub getVmResources {
+    my ($self, %args) = @_;
+
+    General::checkParams(
+        args     => \%args,
+        required => [ 'host' ],
+        optional => { vm => undef, resources => [ 'ram', 'cpu' ] }
+    );
+
+    # If no vm specified, get resssources for all hypervisor vms.
+    my @vms;
+    if (not defined $args{vm}) {
+        @vms = $args{host}->getVms;
+    } else {
+        push @vms, $args{vm};
+    }
+    
+    my $vms_resources = {};
+
+    for my $vm (@vms) {
+        # we get the flavor of the vm
+        my $uuid = $args{host}->openstack_vm_uuid;
+        my $controller = $self->nova_controller;
+        my $details = $controller->api->tenant(id => $controller->api->{tenant_id})
+                          ->servers(id => $uuid)
+                          ->get(target => 'compute');
+
+        my $flavor = $details->{server}->{flavor};
+
+        #get the flavor's details
+        my $f_details = $controller->api->tenant(id => $controller->api->{tenant_id})
+                          ->flavors(id => $flavor->id)
+                          ->get(target => 'compute');
+   
+        my $vm_resource = {};
+        for my $resource (@{ $args{resources} }) {
+
+        }
+
+    }
+
+    return $vms_resources;
+}
+
+1;
