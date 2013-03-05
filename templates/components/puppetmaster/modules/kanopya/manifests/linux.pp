35954adf96325ef0df176943b62d0ec411ea80c2
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2013-03-05 17:23:58 +0100

    [linux] Install ntpdate on every Linux

--- templates/components/puppetmaster/modules/kanopya/manifests/linux.pp
+++ templates/components/puppetmaster/modules/kanopya/manifests/linux.pp
@@ -1,5 +1,5 @@
 class kanopya::linux ($sourcepath) {
-	case $operatingsystem {
+    case $operatingsystem {
         CentOS, Fedora: { $haltpath = "/etc/rc.d/rc0.d/"
                           $netscript = "K[0-9][0-9]network"
                           $iscsiscript = "K[0-9][0-9]iscsi" 
@@ -17,21 +17,30 @@ class kanopya::linux ($sourcepath) {
         
         default:        { fail("Unrecognized operating system") }
     }
+
     file { '/etc/hosts':
-		path    => '/etc/hosts',
-		ensure  => present,
-		mode    => 0644,
-		source  => "puppet:///kanopyafiles/${sourcepath}/etc/hosts",
-	}
-	file { '/etc/resolv.conf':
-		path    => '/etc/resolv.conf',
-		ensure  => present,
-		mode    => 0644,
-		source  => "puppet:///kanopyafiles/${sourcepath}/etc/resolv.conf",
-	}
+        path    => '/etc/hosts',
+        ensure  => present,
+        mode    => 0644,
+        source  => "puppet:///kanopyafiles/${sourcepath}/etc/hosts",
+    }
+
+    file { '/etc/resolv.conf':
+        path    => '/etc/resolv.conf',
+        ensure  => present,
+        mode    => 0644,
+        source  => "puppet:///kanopyafiles/${sourcepath}/etc/resolv.conf",
+    }
+
     tidy {'bad-scripts':
         path    => $haltpath,
         recurse => true,
         matches => [ $iscsiscript, $netscript ]
     }
+
+    package { 'ntpdate':
+        name   => 'ntpdate',
+        ensure => present,
+    }
 }
+
