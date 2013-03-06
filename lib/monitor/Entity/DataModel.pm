6551fa8acb48fbacdb23e50260b23ba4063fdf02
nabila HADIDI <nabila.hadidi@hederatech.com>
2013-03-06 16:57:17 +0100

    [ha] Seasonality detection in Data Model

--- lib/monitor/Entity/DataModel.pm
+++ lib/monitor/Entity/DataModel.pm
@@ -39,6 +39,8 @@ use Kanopya::Exceptions;
 use Log::Log4perl "get_logger";
 use Data::Dumper;
 use List::Util;
+use Statistics::R;
+
 my $log = get_logger("");
 my $errmsg;
 
@@ -298,5 +300,468 @@ sub constructPrediction {
     else {
         return {timestamps => \@timestamps_temp, values => \@predictions};
     }
+}
+
+
+=pod
+
+=begin classdoc
+
+Splits the data into two arrays, one for the times and one for the corresponding values.
+
+@param data the historical data
+
+@return an array reference containing all the times and an array reference of the data values
+
+=end classdoc
+
+=cut
+
+sub splitData {
+    my ($self,%args) = @_;
+
+    General::checkParams(args     => \%args,
+                         required => ['data']
+                         );
+
+    #Split data hashtable in two time-sorted arrays
+    my @time_keys            = keys( %{$args{'data'}} );
+    my @sorted_all_time_keys = sort {$a <=> $b} @time_keys;
+
+    my @sorted_data_values;
+
+    #Keep data in order
+    for my $key (@sorted_all_time_keys) {
+        push @sorted_data_values, $args{data}->{$key};
+    }
+
+    return (\@sorted_all_time_keys, \@sorted_data_values);
+}
+
+
+=pod
+
+=begin classdoc
+
+Computes the autocorrelation by making a call to R (Project for Statistical Computing).
+
+@param data_values the values of the historical data
+@param lag defines the maximum lag for which the acf is computed
+
+@return an array reference which contains the values of the autocorrelation (acf)
+
+=end classdoc
+
+=cut
+
+sub computeACF {
+    my ($self,%args) = @_;
+
+    General::checkParams(args     => \%args,
+                         required => ['data_values','lag']
+                         );
+
+    my $data_values = $args{'data_values'};
+    my $lag         = $args{'lag'};
+
+    my @acf;
+
+    my $path = "/tmp/kanopya/ACF.pdf";
+
+    my $loadvect = "vect <- c (". join(",", @{$data_values}) .")" ;
+
+    # Define an R session
+    my $R = Statistics::R->new();
+
+    # Open an R session
+    $R->startR();
+
+    #Send the instructions to R
+    $R->send(
+            qq`
+            $loadvect
+            pdf("$path")
+            r<-acf(vect,$lag)
+            \n print(r)
+            dev.off()`
+        );
+
+    #Get the results (acf)
+    my $output_value = $R->get('r$acf');
+
+    my $j = 0;
+
+    for (my $i = 7; $i <= $#{$output_value}; $i++) {
+
+        if( $i % 2 != 0 ) {
+            $acf[$j] = ${$output_value}[$i];
+            $j++;
+        }
+    }
+
+    # Close the R session
+    $R->stopR();
+
+    return \@acf;
+}
+
+
+=pod
+
+=begin classdoc
+
+Computes the value IC of the confidence interval [-IC,+IC] for a confidence of 95%.
+
+@param data_values the values of the historical data
+
+@return the value of the confidence interval
+
+=end classdoc
+
+=cut
+
+sub confidenceAutocorrelation {
+    my ($self,%args) = @_;
+
+    General::checkParams(args     => \%args,
+                         required => ['data_values']
+                         );
+
+
+    my $data_values = $args{'data_values'};
+
+    #The formula is 2/square root(cardinality of data_values)
+    my $IC = 2/sqrt($#{$data_values}+1);
+
+    return $IC;
+}
+
+
+=pod
+
+=begin classdoc
+
+Detects the peaks of the values given in an array by taking only the concave ones for which
+the value is higher than the value of the confidence interval.
+
+@param IC the value for the confidence interval [-IC,+IC]
+@param tab the values for which the peaks are detected
+
+@return an array references of the positions (array index) of the peaks
+
+=end classdoc
+
+=cut
+
+
+sub detectPeaks {
+    my ($self,%args) = @_;
+
+    General::checkParams(args     => \%args,
+                         required => ['IC','tab']
+                         );
+
+
+    my $IC  = $args{'IC'};
+    my $tab = $args{'tab'};
+
+    my $data_count    = $#{$tab}+1;
+    my $previousSlope = 0;
+    my @peaks;
+
+    #For each value of the array $tab
+    for (my $i = 1; $i < $data_count; $i++) {
+
+        my $slope = $tab->[$i] - $tab->[$i-1];
+
+        #Take the peaks of the array :the greatest positive values or the smallest negative values
+        #If slope*previousSlope<0 it means there is a peak, in our case we take only the concave ones
+        #for which the values are > $IC
+        if ( ($slope*$previousSlope < 0) && ($tab->[$i-1] > $tab->[$i]) && ($tab->[$i-1] > $IC) ) {
+            push @peaks, ($i-1);
+        }
+
+        $previousSlope = $slope;
+    }
+
+    $log->debug("Peaks @peaks \n");
+
+    return \@peaks;
+}
+
+
+
+=pod
+
+=begin classdoc
+
+Searches if there is also a peak for multiple values of a given peak. This method accepts a given
+offset when searching the periodic peaks.
+
+@param pos the position (array index) of peaks array for which the periodicity is computed
+@param acf an array containing the values of the autocorrelation
+@param peaks the positions of the peaks of the acf array
+
+@return the maximum number of periodicity, the minimum value of the autocorrelation function of the
+periodically peaks and the estimated value of the seasonality
+
+=end classdoc
+
+=cut
+
+
+sub detectPeriodicity {
+    my ($self,%args) = @_;
+
+    General::checkParams(args     => \%args,
+                         required => ['pos','acf','peaks']
+                         );
+
+
+    my $pos     = $args{'pos'};
+    my $acf     = $args{'acf'};
+    my $peaks   = $args{'peaks'};
+
+
+    #Will contain the approximated value of the seasonality with its frequency
+    my %value_peak;
+    $value_peak {$peaks->[$pos]+1} = 1;
+
+    my $multiple = 2;
+
+    #Possible offset when searching periodicity of the peak
+    my $offset  = int(($#{$acf}+1)*0.04);
+    my $min_max = $acf->[$peaks->[$pos]];
+
+    for (my $i = $pos+1; $i < $#{$peaks}+1; $i++) {
+
+        #Search for the multiple value of lag given by $peak->[$pos]+1
+        if ( (($peaks->[$i]+1) <= ($multiple*($peaks->[$pos]+1)+$offset)) &&
+             (($peaks->[$i]+1) >= ($multiple*($peaks->[$pos]+1)-$offset)) ) {
+
+            #Estimates the seasonality
+            my $round = int( (($peaks->[$i]+1)/$multiple)+0.5 );
+
+            if ( exists( $value_peak{$round} ) ) {
+                $value_peak{$round}++;
+            }
+            else {
+                $value_peak{$round} = 1;
+            }
+
+            $multiple++;
+
+            #Change the min_max value of the acf
+            if ($acf->[$peaks->[$i]] < $min_max) {
+                $min_max = $acf->[$peaks->[$i]];
+            }
+        }
+
+        #Out if the current element is higher than what expecting
+        else {
+            last if ($peaks->[$i]+1 > $multiple*($peaks->[$pos]+1)+$offset);
+       }
+    }
+
+    #Computes the seasonality that has a maximal frequency
+    my $mode_value_peak = $peaks->[$pos]+1;
+
+    while ( my ($k,$v) = each(%value_peak) ) {
+        if ($value_peak{$k} > $value_peak{$mode_value_peak}) {
+            $mode_value_peak = $k;
+        }
+    }
+
+    $log->debug("Multiple ". $pos ." -> ". $multiple." \n");
+    return ($multiple-1, $min_max, $mode_value_peak);
+}
+
+
+=pod
+
+=begin classdoc
+
+Computes the seasonality by using spectral density.
+
+@param data_values the values of the historical data
+
+@return the seasonality value (if equal to one, it means that there is no seasonality)
+
+=end classdoc
+
+=cut
+
+
+sub findSeasonalityDSP {
+    my ($self,%args) = @_;
+
+    General::checkParams(args     => \%args,
+                         required => ['data_values']
+                         );
+
+
+    my $data_values = $args{'data_values'};
+
+    # Define the R session
+    my $R = Statistics::R->new();
+
+    # Open the R session
+    $R->startR();
+
+    # Introduces , into the series (R format)
+    my $loadvect = "vect <- c (". join(",", @{$data_values}) .")" ;
+
+    my $nameFile = "/opt/kanopya/scripts/R/findFreq.R";
+    open (FILE,"<$nameFile") or die"open: $!";
+
+    my @find_freq = <FILE>;
+    my $find_freq = join("",@find_freq);
+    close FILE;
+
+    $R->send(
+            qq`
+            $find_freq
+            $loadvect
+            r<-find.freq(vect)
+            \n print(r)`
+        );
+
+    my $saisonal = $R->get('r');
+
+    # Close the R session
+    $R->stopR();
+
+    $log->debug('Approach 1 : DSP (if 1 it means no seasonality) '.$saisonal." points \n");
+
+    return $saisonal;
+}
+
+
+
+=pod
+
+=begin classdoc
+
+Computes the possible seasonalities based on the autocorrelation (acf).
+
+@param data_values the values of the historical data
+
+@return an array reference of the seasonality values
+
+=end classdoc
+
+=cut
+
+sub findSeasonalityACF {
+    my ($self, %args) = @_;
+
+    General::checkParams(args     => \%args,
+                         required => ['data_values']
+                         );
+
+
+    my $data_values = $args{'data_values'};
+
+    #Contains the possible seasonalities obtained by acf
+    my @season;
+
+    #Contains the mininum autocorrelation value for each seasonality
+    my @min_max_acf;
+
+    #A choice : contains the lag for autocorrelation
+    my $lag = int( ($#{$data_values}+1)/2+1 );
+
+     #Returns the ACF of the data values with the specific lag
+    my $acf = $self->computeACF('data_values' => $data_values, 'lag' => $lag);
+
+    #Computes the value of the acf for a confidence of 95%
+    my $IC = $self->confidenceAutocorrelation('data_values' => $data_values);
+
+    #Approach 2 :ACF
+    $log->debug("Approach 2 : ACF");
+
+    #Computes the peaks of the acf
+    my $peaks = $self->detectPeaks('IC' => $IC,'tab' => $acf);
+
+    #A call to the detectPeriodicity with each position of the @peaks array
+    for (my $pos = 0; $pos < $#{$peaks}+1; $pos++) {
+
+        my ($multiple, $min_max, $mode_value_peak) =
+        $self->detectPeriodicity('pos' => $pos, 'acf' => $acf, 'peaks' => $peaks);
+
+        #If we have as many multiple as necessary for $lag,
+        #put seasonality into @season and save $min_max corresponding
+        my $nb_period = int($lag/$mode_value_peak);
+
+        my $err = int($nb_period*0.6);
+
+        if ($multiple >= $nb_period-$err) {
+
+            push @season, $mode_value_peak;
+            push @min_max_acf, $min_max;
+
+        }
+    }
+
+    my @sorted_season;
+
+    if (scalar @season != 0) {
+
+        $log->debug('The seasonalities and corresponding min autocorrelations found are:');
+        $log->debug("points @season \n");
+        $log->debug("value acf @min_max_acf \n");
+
+        #Sort the values of the seasonalities following the values of $min_max_acf
+        my %h = map { $season[$_] => $min_max_acf[$_]} (0..$#season);
+
+        @sorted_season = sort { $h{$b} <=> $h{$a} } keys %h;
+    }
+
+    return \@sorted_season;
+}
+
+
+=pod
+
+=begin classdoc
+
+Computes the possible values of the seasonality by using the acf and spectral density approaches.
+
+@param data the historical data
+@return an array reference of the values of the seasonalities
+
+=end classdoc
+
+=cut
+
+
+#Call the approaches to find the seasonalities
+sub findSeasonality {
+
+    #Get the time serie in the form of a hashTable
+    my ($self, %args) = @_;
+
+    General::checkParams(args     => \%args,
+                         required => ['data']
+                         );
+
+    #Get the data values of the time serie in an array
+    my ($data_time, $data_values) = $self->splitData('data' => $args{'data'});
+
+    my @season;
+    my $seasonal_DSP = $self->findSeasonalityDSP('data_values' => $data_values);
+    my $season_ACF   = $self->findSeasonalityACF('data_values' => $data_values);
+
+    if ( ($seasonal_DSP != 1) && ((grep {$_ eq $seasonal_DSP} @{$season_ACF}) == 0) ) {
+        push @season, $seasonal_DSP;
+    }
+
+    if (scalar @$season_ACF != 0) {
+        push  @season, @{$season_ACF};
+    }
+
+    $log->debug("The seasonalities @season \n");
+
+    return \@season;
 }
 1;
