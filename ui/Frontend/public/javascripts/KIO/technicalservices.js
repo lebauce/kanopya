0697b92be11349524844b603036f78ed93e9874f
BAH Souleymane <sbah@hedera-technology.com>
2013-03-27 17:34:43 +0100

    [ui] add an "Initializing configuration" message after addTechnicalService validation

--- ui/Frontend/public/javascripts/KIO/technicalservices.js
+++ ui/Frontend/public/javascripts/KIO/technicalservices.js
@@ -97,7 +97,24 @@ function connectConnectorForm(eid, type, id, cb) {
                 name    : tmp,
                 fields  : fields,
                 cancel  : function() { deleteService(eid); },
-                callback: function() { $('#technicalserviceslistgrid').trigger('reloadGrid'); cb(); }
+                beforeSubmit: function() {
+                    setTimeout(function() {
+                        var dialog = $("<div>", { id : "waiting_default_insert", title : "Initializing configuration", text : "Please wait..." });
+                        dialog.css('text-align', 'center');
+                        dialog.appendTo("body").dialog({
+                            resizable   : false
+                        });
+                        $(dialog).parents('div.ui-dialog').find('span.ui-icon-closethick').remove();
+                    }, 10);
+                },
+                callback    : function(data) {
+                    $("div#waiting_default_insert").dialog("destroy");
+                    $('#technicalserviceslistgrid').trigger('reloadGrid');
+                    cb();
+                },
+                error       : function(data) {
+                    $("div#waiting_default_insert").dialog("destroy");
+                }
             });
             $(modal.form).remove();
             modal.form  = tmpmod.form;
