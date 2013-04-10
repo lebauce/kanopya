35632240a022d12ad1b0852a8b54e806d7df74c5
Paul Joannon <pjoannon@hederatech.com>
2013-04-10 16:29:16 +0200

    [mysql] raise an error when applying kanopya::mysql puppet class on an unsupported operatingsystem

--- templates/components/puppetmaster/modules/kanopya/manifests/mysql.pp
+++ templates/components/puppetmaster/modules/kanopya/manifests/mysql.pp
@@ -1,39 +1,57 @@
-class kanopya::mysql::repo {
-    if $operatingsystem =~ /(?i)(centos)/ {
-        yumrepo { 'MariaDB':
-            baseurl  => 'http://yum.mariadb.org/5.5/centos6-amd64',
-            enabled  => '1',
-            gpgcheck => '1',
-            gpgkey   => 'https://yum.mariadb.org/RPM-GPG-KEY-MariaDB'
-        }
+class kanopya::mysql::deb($config_hash) {
+    $release = $operatingsystem ? {
+        /(?i)(debian)/ => 'squeeze',
+        /(?i)(ubuntu)/ => 'precise'
     }
-    elsif $operatingsystem =~ /(?i)(debian|ubuntu)/ {
-        $release = $operatingsystem ? {
-            'Debian' => 'squeeze',
-            'Ubuntu' => 'precise'
-        }
-        apt::source { 'MariaDB':
-            location   => 'http://ftp.igh.cnrs.fr/pub/mariadb/repo/5.5/${operatingsystem}',
-            release    => $release,
-            repos      => 'main',
-            key        => '0xcbcb082a1bb943db',
-            key_server => 'keyserver.ubuntu.com'
-        }
+    $os = downcase($operatingsystem)
+    apt::source { 'MariaDB':
+        location   => "http://ftp.igh.cnrs.fr/pub/mariadb/repo/5.5/${os}",
+        release    => $release,
+        repos      => 'main',
+        key        => 'cbcb082a1bb943db',
+        key_server => 'keyserver.ubuntu.com',
+        before     => Class['::mysql::server']
+    }
+    class { '::mysql::server':
+        service_name => 'mysql',
+        config_hash  => $config_hash,
+        package_name => 'mariadb-galera-server',
+        require      => Apt::Source['MariaDB']
+    }
+}
+
+class kanopya::mysql::rh($config_hash) {
+    yumrepo { 'MariaDB':
+        baseurl  => 'http://yum.mariadb.org/5.5/centos6-amd64',
+        enabled  => '1',
+        gpgcheck => '1',
+        gpgkey   => 'https://yum.mariadb.org/RPM-GPG-KEY-MariaDB',
+        before   => Class['::mysql::server']
+    }
+    class { '::mysql::server':
+        service_name  => 'mysql',
+        config_hash   => $config_hash,
+        package_name  => 'MariaDB-Galera-server',
+        require       => Yumrepo['MariaDB']
     }
 }
 
 class kanopya::mysql($config_hash) {
-    class { 'kanopya::mysql::repo':
-        before => Class['mysql::server']
+    case $operatingsystem {
+        /(?i)(debian|ubuntu)/ : {
+            class { 'kanopya::mysql::deb':
+                config_hash => $config_hash
+            }
+        }
+        /(?i)(centos)/ : {
+            class { 'kanopya::mysql::rh':
+                config_hash => $config_hash
+            }
+        }
+        default : {
+            fail("Unsupported operatingsystem : ${operatingsystem}. Only Debian, Ubuntu and CentOS are supported")
+        }
     }
-
-    class { 'mysql::server':
-        package_name => 'MariaDB-Galera-server',
-        service_name => 'mysql',
-        config_hash  => $config_hash,
-        require      => Class['kanopya::mysql::repo']
-    }
-
     Mysql::Db <<| tag == "${fqdn}" |>>
     Database_user <<| tag == "${fqdn}" |>>
     Database_grant <<| tag == "${fqdn}" |>>
