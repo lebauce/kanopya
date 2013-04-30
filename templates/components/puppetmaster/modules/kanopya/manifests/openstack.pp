5f712b3c7208e1a294511c004855191132d5c554
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2013-04-30 14:46:15 +0200

    [openstack] Update keystone module for Grizzly

--- templates/components/puppetmaster/modules/kanopya/manifests/openstack.pp
+++ templates/components/puppetmaster/modules/kanopya/manifests/openstack.pp
@@ -7,16 +7,16 @@ class kanopya::openstack::repository {
 
         apt::source { 'ubuntu-cloud-repository':
             location => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
-            release  => 'precise-updates/folsom',
+            release  => 'precise-updates/grizzly',
             repos    => 'main',
         }
     }
 }
 
-class kanopya::keystone($dbserver, $password) {
+class kanopya::keystone($dbserver, $dbip, $dbpassword, $adminpassword, $email) {
     @@mysql::db { 'keystone':
         user     => 'keystone',
-        password => "${password}",
+        password => "${dbpassword}",
         host     => "${ipaddress}",
         grant    => ['all'],
         tag      => "${dbserver}",
@@ -26,6 +26,26 @@ class kanopya::keystone($dbserver, $password) {
         public_address   => "${fqdn}",
         admin_address    => "${fqdn}",
         internal_address => "${fqdn}",
+    }
+
+    class { 'keystone::roles::admin':
+        email        => "${email}",
+        password     => "${adminpassword}",
+        require      => Exec['/usr/bin/keystone-manage db_sync'],
+        admin_tenant => 'openstack'
+    }
+
+    class { '::keystone':
+        verbose        => true,
+        debug          => true,
+        sql_connection => "mysql://keystone:${dbpassword}@${dbserver}/keystone",
+        catalog_type   => 'sql',
+        admin_token    => 'admin_token',
+        before => Class['keystone::roles::admin'],
+    }
+
+    exec { "/usr/bin/keystone-manage db_sync":
+        path => "/usr/bin:/usr/sbin:/bin:/sbin",
     }
 
     Keystone_user <<| tag == "${fqdn}" |>>
