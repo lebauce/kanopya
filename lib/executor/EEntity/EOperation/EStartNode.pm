a790b3a2ee531280114e260d2f262fd6ba4ef6c6
Paul Joannon <pjoannon@hederatech.com>
2013-05-07 12:38:52 +0200

    [linux] check harddisk's service_provider_id before switching used initrd at boot time

--- lib/executor/EEntity/EOperation/EStartNode.pm
+++ lib/executor/EEntity/EOperation/EStartNode.pm
@@ -240,11 +240,22 @@ sub _generateBootConf {
                   );
         }
         my $host_params = $cluster->getManagerParameters(manager_type => 'HostManager');
+        $kernel_version = Entity::Kernel->get(id => $kernel_id)->kernel_version;
         if ($host_params->{deploy_on_disk}) {
-            $kernel_version  = Entity::Kernel->find(hash => { kernel_name => 'deployment' })->kernel_version;
-        }
-        else {
-            $kernel_version  = Entity::Kernel->get(id => $kernel_id)->kernel_version;
+            eval {
+                my $harddisk = $args{host}->findRelated(
+                    filters  => [ 'harddisks' ],
+                    order_by => 'harddisk_device'
+                );
+                if ($harddisk->service_provider_id != $args{cluster}->id) {
+                    $kernel_version = Entity::Kernel->find(hash => { kernel_name => 'deployment' })->kernel_version;
+                }
+            };
+            if ($@) {
+                throw Kanopya::Exception::Internal::NotFound(
+                    error => "No hard disk to deploy the system on was found"
+                );
+            }
         }
 
         my $linux_component = EEntity->new(entity => $cluster->getComponent(category => "System"));
