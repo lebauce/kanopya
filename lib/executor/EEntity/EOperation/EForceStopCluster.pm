462eeb4d3f71b8026a6301ec14f7382ff65a2c3b
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2013-05-16 17:56:57 +0200

    [core] Fix forceStopCluster

--- lib/executor/EEntity/EOperation/EForceStopCluster.pm
+++ lib/executor/EEntity/EOperation/EForceStopCluster.pm
@@ -56,33 +56,35 @@ sub execute {
     my $subnet = $self->{context}->{component_dhcpd}->getInternalSubNetId();
 
     foreach my $node (reverse $self->{context}->{cluster}->nodesByWeight()) {
+        my $ehost = EEntity->new(data => $node->host);
         eval {
             # Halt Node
-            my $ehost = EEntity->new(data => $node);
             $ehost->halt();
         };
         if ($@) {
             my $error = $@;
-            $errmsg = "Problem with node <" . $node->host_id . "> during force stop cluster : $error";
+            $errmsg = "Problem with node <" . $node->host->host_id . "> during force stop cluster : $error";
             $log->warn($errmsg);
         }
 
         eval {
             # Update Dhcp component conf
-            my $host_mac = $node->getPXEIface->iface_mac_addr;
+            my $host_mac = $node->host->getPXEIface->iface_mac_addr;
             if ($host_mac) {
-	            my $hostid = $self->{context}->{component_dhcpd}->getHostId(
-	                             dhcpd3_subnet_id         => $subnet,
-	                             dhcpd3_hosts_mac_address => $host_mac
-	                         );
+                my $hostid = $self->{context}->{component_dhcpd}->getHostId(
+                                 dhcpd3_subnet_id         => $subnet,
+                                 dhcpd3_hosts_mac_address => $host_mac
+                             );
 
-	            $self->{context}->{component_dhcpd}->removeHost(dhcpd3_subnet_id => $subnet,
-	                                                            dhcpd3_hosts_id  => $hostid);
+                $self->{context}->{component_dhcpd}->removeHost(
+                    dhcpd3_subnet_id => $subnet,
+                    dhcpd3_hosts_id  => $hostid
+                );
             }
         };
         if ($@) {
             my $error = $@;
-            $errmsg = "Problem with node <" . $node->getAttr(name=>"host_id").
+            $errmsg = "Problem with node <" . $node->host->id .
                       "> during dhcp configuration update : $error";
             $log->warn($errmsg);
         }
@@ -93,12 +95,14 @@ sub execute {
         my @components = $self->{context}->{cluster}->getComponents(category => "all");
         foreach my $component (@components) {
             EEntity->new(data => $component)->cleanNode(
-                host => $node, mount_point => '', cluster => $self->{context}->{cluster}
+                host        => $ehost,
+                mount_point => '',
+                cluster     => $self->{context}->{cluster}
             );
         }
 
-        $node->node->setAttr(name => "node_hostname", value => undef, save => 1);
-        $node->setAttr(name => "host_initiatorname", value => undef, save => 1);
+        $node->node_hostname = undef;
+        $node->host->host_initiatorname = undef;
 
         $self->{context}->{cluster}->unregisterNode(node => $node);
     }
