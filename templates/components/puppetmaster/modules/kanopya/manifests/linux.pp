61c19d86b911af890c7d7134f60a1bd9bfcb2a02
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2013-05-31 10:44:32 +0200

    [ubuntu] Force updating repositories after having installed ubuntu-cloud-keyring

--- templates/components/puppetmaster/modules/kanopya/manifests/linux.pp
+++ templates/components/puppetmaster/modules/kanopya/manifests/linux.pp
@@ -71,6 +71,13 @@ class kanopya::linux ($sourcepath) {
             require => Exec['apt-get update']
         }
 
+        exec { 'apt-get -q update':
+            path        => '/usr/bin',
+            tries       => 5,
+            subscribe   => Package['ubuntu-cloud-keyring'],
+            refreshonly => true
+        }
+
         file { '/etc/apt/sources.list.d/ubuntu-cloud-repository.list':
             content => "# ubuntu-cloud-repository\ndeb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/grizzly main\ndeb-src http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/grizzly main",
             notify  => Exec['apt_update'],
