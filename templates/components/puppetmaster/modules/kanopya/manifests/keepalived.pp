8050acd42ea568c44cc8a5b6ef7b6dcb2c1e32a8
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2013-06-07 16:48:13 +0200

    [keepalived] Base the Kanopya puppet module on the one from puppetlabs

--- templates/components/puppetmaster/modules/kanopya/manifests/keepalived.pp
+++ templates/components/puppetmaster/modules/kanopya/manifests/keepalived.pp
@@ -1,28 +1,10 @@
-class kanopya::keepalived::service {
-	service {
-		'keepalived':
-			name => $operatingsystem ? {
-				default => 'keepalived'
-			},
-			ensure => running,
-			hasstatus => true,
-			hasrestart => true,
-			enable => true,
-			require => Class['kanopya::keepalived::install'],
+class kanopya::keepalived(
+    email,
+    smtp_server
+) {
+	class { 'concat::setup': }
+	class { '::keepalived':
+		email => "${email}",
+		smtp_server => "${smtp_server}"
 	}
 }
-
-class kanopya::keepalived::install {
-	package {
-		'keepalived':
-			name => $operatingsystem ? {
-				default => 'keepalived'
-			},
-			ensure => present,
-	}
-}
-
-class kanopya::keepalived {
-	include kanopya::keepalived::install, kanopya::keepalived::service
-}
-
