0e113c95f39209ac80acc616bcb14e7f2261d844
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2013-06-12 18:53:10 +0200

    [setup] Fix Puppet tag for AMQP

--- scripts/install/setup.pl
+++ scripts/install/setup.pl
@@ -836,7 +836,7 @@ sub generatePuppetConfiguration {
     system('/etc/init.d/puppet', 'restart');
     system('/etc/init.d/puppetmaster', 'restart');
 
-    EEntity->new(entity => $kanopya)->reconfigure(tags => [ "system", "rabbitmq" ]);
+    EEntity->new(entity => $kanopya)->reconfigure(tags => [ "system", "kanopya::amqp" ]);
 }
 
 
