b9ca4a0b49890b3e3abac331d1f8aa2679cc23e9
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2013-06-24 02:39:35 +0200

    [openstack] Remove duplicate declaration in quantum puppet module

--- templates/components/puppetmaster/modules/kanopya/manifests/openstack/quantum/common.pp
+++ templates/components/puppetmaster/modules/kanopya/manifests/openstack/quantum/common.pp
@@ -11,8 +11,4 @@ class kanopya::openstack::quantum::common(
         rabbit_user         => "${rabbit_user}",
         rabbit_virtual_host => "${rabbit_virtualhost}"
     }
-
-    quantum_plugin_ovs {
-        'SECURITYGROUP/firewall_driver': value => "quantum.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver";
-    }
 }
