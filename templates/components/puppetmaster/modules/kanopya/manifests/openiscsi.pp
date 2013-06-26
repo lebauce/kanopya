e6ad963b4cfff66525becac16430c163d8ab5a61
Paul Joannon <pjoannon@hederatech.com>
2013-06-26 18:23:38 +0200

    [openiscsi] handle configuration with puppet

--- templates/components/puppetmaster/modules/kanopya/manifests/openiscsi.pp
+++ templates/components/puppetmaster/modules/kanopya/manifests/openiscsi.pp
@@ -1,30 +1,8 @@
-class kanopya::openiscsi::service {
-	service {
-		'openiscsi':
-			name => $operatingsystem ? {
-				/(Red Hat|CentOS|Fedora)/ => 'iscsid',
-				default => 'open-iscsi'
-			},
-			ensure => running,
-			hasstatus => true,
-			hasrestart => true,
-			enable => true,
-			require => Class['kanopya::openiscsi::install'],
-	}
-}
-
-class kanopya::openiscsi::install {
-	package {
-		'openiscsi':
-			name => $operatingsystem ? {
-				/(Red Hat|CentOS|Fedora)/ => 'iscsi-initiator-utils',
-				default => 'open-iscsi'
-			},
-			ensure => present,
-	}
-}
-
-class kanopya::openiscsi {
-	include kanopya::openiscsi::install, kanopya::openiscsi::service
+class kanopya::openiscsi(
+    $initiatorname
+){
+        file { '/etc/iscsi/initiatorname.iscsi':
+            content => "InitiatorName=${initiatorname}\n"
+        }
 }
 
