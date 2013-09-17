0208ebdac84b74ba45ffb27e477e545a42d7db6c
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2013-09-17 16:46:23 +0200

    [puppet] Handle buggy RabbitMQ module

--- templates/components/puppetmaster/modules/kanopya/manifests/rabbitmq.pp
+++ templates/components/puppetmaster/modules/kanopya/manifests/rabbitmq.pp
@@ -14,13 +14,13 @@ class kanopya::rabbitmq ($disk_nodes, $cookie) {
     class { 'rabbitmq::server':
         wipe_db_on_cookie_change => true,
         config_cluster           => true,
+        config_mirrored_queues   => $operatingsystem ? {
+            /(?i)(centos|redhat|fedora)/ => false,
+            default                      => true
+        },
         cluster_disk_nodes       => $disk_nodes,
         erlang_cookie            => $cookie,
-        require                  => Class["$rabbitmq_repo"],
-        package_name             => $operatingsystem ? {
-            /(?i)(centos|redhat|fedora)/ => 'rabbitmq-server',
-            default                      => 'rabbitmq-server'
-        }
+        require                  => Class["$rabbitmq_repo"]
     }
     Rabbitmq_user <<| tag == "${fqdn}" |>>
     Rabbitmq_user_permissions <<| tag == "${fqdn}" |>>
