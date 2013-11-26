41e8c6816622bb6e9aacacfb8c99583cf59dc49e
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2013-11-26 18:57:47 +0100

    [basedb] Use primary keys to detect inheritance relations

--- lib/administrator/BaseDB.pm
+++ lib/administrator/BaseDB.pm
@@ -1464,12 +1464,14 @@ sub _attributesDefinition {
         # Complete the attrdef with regular relations, expecting inheritance and many to many
         for my $relation ($schema->relationships) {
             my $relinfo = $schema->relationship_info($relation);
+            my $relschema = BaseDB->_resultSource(classname => BaseDB->_className(class => $relinfo->{source}));
+            my $relpkname = BaseDB->_primaryKeyName(schema => $relschema);
 
             # Deduce the key and fk of the relation from definition
             (my $key = (keys %{ $relinfo->{cond} })[0]) =~ s/.*foreign\.//g;
             (my $fk = (values %{ $relinfo->{cond} })[0]) =~ s/.*self\.//g;
 
-            if (($fk ne $pkname || (($fk eq $key || $args{include_reverse}) &&
+            if (($fk ne $pkname || (($relpkname ne $key || $args{include_reverse}) &&
                 (! $relinfo->{attrs}->{is_foreign_key_constraint}))) &&
                 (! $relinfo->{attrs}->{cascade_update} || $key eq $pkname)) {
                 if (! defined $attributedefs->{$modulename}->{$relation}) {
@@ -1513,7 +1515,9 @@ sub _attributesDefinition {
         # Keep the module belonging for each attributes
         for my $attrname (keys %{ $attributedefs->{$module} }) {
             my $definition = $attributedefs->{$module}->{$attrname};
-            if (! (defined $result->{$attrname} && defined $definition->{specialized})) {
+            if (! (defined $result->{$attrname}
+                && defined $definition->{specialized})
+                && ! defined $result->{$attrname}->{from_module}) {
                 $definition->{from_module} = $module;
             }
         }
@@ -1819,7 +1823,16 @@ sub _relationshipInfos {
     my $relation_class = BaseDB->_dbixClass(schema => $relation_schema);
 
     # Deduce the fk from relation definition
-    my @conds = keys %{ $schema->relationship_info($args{relation})->{cond} };
+    my @conds;
+    try {
+        @conds = keys %{ $schema->relationship_info($args{relation})->{cond} };
+    }
+    catch ($err) {
+        throw Kanopya::Exception::Internal::NotFound(
+                  error => "Could not find the relation $args{relation} on " . $schema->from()
+              );
+    }
+
     (my $fk = $conds[0]) =~ s/.*foreign\.//g;
 
     my $infos = {
