2496da2fe081e9fd414127e3682ab2d63a121617
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2013-11-30 02:41:02 +0100

    [puppet] Fix duplicate package alias and NFS module cleanup

--- templates/components/puppetmaster/modules/kanopya/manifests/nfs.pp
+++ templates/components/puppetmaster/modules/kanopya/manifests/nfs.pp
@@ -1,29 +1,27 @@
 class kanopya::nfs::service {
-	if $operatingsystem =~ /(RedHat|CentOS|Fedora)/ {
-		service {
-			'nfs':
-				name => 'rpcbind',
-				ensure => running,
-				hasstatus => true,
-				hasrestart => true,
-				enable => true,
-				require => Class['kanopya::nfs::install'],
-		}
-	}
+  if $operatingsystem =~ /(RedHat|CentOS|Fedora)/ {
+    service { 'rpcbind':
+        name => 'rpcbind',
+        ensure => running,
+        hasstatus => true,
+        hasrestart => true,
+        enable => true,
+        require => Class['kanopya::nfs::install'],
+    }
+  }
 }
 
 class kanopya::nfs::install {
-	package {
-		'nfs':
-			name => $operatingsystem ? {
-				/(RedHat|CentOS|Fedora)/ => 'nfs-utils',
-				default => 'nfs-common'
-			},
-			ensure => present,
-	}
+  package { 'nfs':
+      name => $operatingsystem ? {
+        /(RedHat|CentOS|Fedora)/ => 'nfs-utils',
+        default => 'nfs-common'
+      },
+      ensure => present,
+  }
 }
 
 class kanopya::nfs {
-	include kanopya::nfs::install, kanopya::nfs::service
+  include kanopya::nfs::install, kanopya::nfs::service
 }
 
