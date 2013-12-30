1f9fda0a721a9fbb1df2c5cb630f9aa649c897af
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2013-12-30 13:03:16 +0100

    [libguestfs] Update Puppet module for Puppet 3.4

--- templates/components/puppetmaster/modules/kanopya/manifests/fileimagemanager.pp
+++ templates/components/puppetmaster/modules/kanopya/manifests/fileimagemanager.pp
@@ -1,19 +1,30 @@
 class kanopya::fileimagemanager {
-  package { 'libguestfs':
-    name => $operatingsystem ? {
-      /(RedHat|CentOS|Fedora)/ => [ 'libguestfs', 'libguestfs-tools', 'qemu-img' ],
-      default => [ 'libguestfs0', 'guestmount', 'qemu-utils', 'libguestfs-tools' ]
-    },
+  $libguestfs_packages = $operatingsystem ? {
+    /(RedHat|CentOS|Fedora)/ => [
+      'libguestfs',
+      'libguestfs-tools',
+      'qemu-img'
+    ],
+    default                  => [
+      'libguestfs0',
+      'guestmount',
+      'qemu-utils',
+      'libguestfs-tools'
+    ]
+  }
+
+  package { $libguestfs_packages:
     audit  => all,
     ensure => present,
-    notify => Exec['update-guestfs-appliance']
+    notify => Exec['update-guestfs-appliance'],
+    before => Exec['update-guestfs-appliance'],
   }
 
   exec { 'update-guestfs-appliance':
     path        => "/usr/bin:/usr/sbin:/bin:/sbin",
     refreshonly => true,
-    subscribe   => Package['libguestfs'],
-    require     => Package['libguestfs'],
+    subscribe   => Package[$libguestfs_packages],
+    require     => Package[$libguestfs_packages],
     command     => $operatingsystem ? {
       /(RedHat|CentOS|Fedora)/ => 'true',
       default                  => 'update-guestfs-appliance'
