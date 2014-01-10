20aac1d60656ee05ea1b2053c52c100a6a798d03
Sylvain Baubeau <sylvain.baubeau@hederatech.com>
2014-01-10 17:04:07 +0100

    [openstack] Move Ubuntu cloud repositories to kanopya::openstack::repo

--- templates/components/puppetmaster/modules/kanopya/manifests/linux.pp
+++ templates/components/puppetmaster/modules/kanopya/manifests/linux.pp
@@ -90,26 +90,6 @@ class kanopya::linux (
     Apt::Source <| |> -> Exec['apt-get update'] -> Package <| |>
   }
 
-  if $operatingsystem =~ /(?i)(ubuntu)/ {
-    package { 'ubuntu-cloud-keyring':
-      name    => 'ubuntu-cloud-keyring',
-      ensure  => present,
-      require => Exec['apt-get update']
-    }
-
-    exec { 'apt-get -q update':
-      path        => '/usr/bin',
-      tries       => 5,
-      subscribe   => Package['ubuntu-cloud-keyring'],
-      refreshonly => true
-    }
-
-    file { '/etc/apt/sources.list.d/ubuntu-cloud-repository.list':
-      content => "# ubuntu-cloud-repository\ndeb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/grizzly main\ndeb-src http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/grizzly main",
-      require => Package['ubuntu-cloud-keyring']
-    }
-  }
-
   create_resources('file', $files)
   create_resources('mount', $mounts)
   create_resources('swap', $swaps)
--- templates/components/puppetmaster/modules/kanopya/manifests/openstack/repository.pp
+++ templates/components/puppetmaster/modules/kanopya/manifests/openstack/repository.pp
@@ -1,2 +1,21 @@
 class kanopya::openstack::repository {
+  if $operatingsystem =~ /(?i)(ubuntu)/ {
+    package { 'ubuntu-cloud-keyring':
+      name    => 'ubuntu-cloud-keyring',
+      ensure  => present,
+      require => Exec['apt-get update']
+    }
+
+    exec { 'apt-get -q update':
+      path        => '/usr/bin',
+      tries       => 5,
+      subscribe   => Package['ubuntu-cloud-keyring'],
+      refreshonly => true
+    }
+
+    file { '/etc/apt/sources.list.d/ubuntu-cloud-repository.list':
+      content => "# ubuntu-cloud-repository\ndeb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/grizzly main\ndeb-src http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/grizzly m
+      require => Package['ubuntu-cloud-keyring']
+    }
+  }  
 }
