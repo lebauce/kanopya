7a19503465c5978f17efe7c266a13c35db866d47
Quentin Chergui <qchergui@hedera-technology.com>
2014-01-30 14:46:51 +0100

    [puppet] Change MariaDB repository address

--- templates/components/puppetmaster/modules/kanopya/manifests/mysql.pp
+++ templates/components/puppetmaster/modules/kanopya/manifests/mysql.pp
@@ -94,7 +94,7 @@ class kanopya::mysql::repos::deb {
   }
 
   @apt::source { 'MariaDB':
-    location   => "http://ftp.osuosl.org/pub/mariadb/mariadb-5.5.32/repo/${os}",
+    location   => "http://mariadb.mirror.nucleus.be/repo/5.5/${os}",
     release    => $release,
     repos      => 'main',
     key        => 'cbcb082a1bb943db',
