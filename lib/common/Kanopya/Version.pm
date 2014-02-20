cb0b79c076016c45f04663142f0bc9bd1dbd52cd
Kevin Pouget <kevin.pouget@hederatech.com>
2014-02-20 14:44:12 +0100

    [version] Bump to version 1.10.2

--- lib/common/Kanopya/Version.pm
+++ lib/common/Kanopya/Version.pm
@@ -34,7 +34,7 @@ use version;
 # The Kanopya version numbers fit the following convention:
 # (Major version).(Minor version).(Iteration number)[.(Hotfix number)]
 
-our $VERSION = version->declare("1.10.1");
+our $VERSION = version->declare("1.10.2");
 
 
 =pod
