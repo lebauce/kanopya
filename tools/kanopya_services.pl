37aea610e3b26ef0a99d79485d26bbebaae542d0
Viktor Horvath <viktor.horvath@hederatech.com>
2014-04-22 20:18:21 +0200

    Do not load the state-manager among the HCM services.

--- tools/kanopya_services.pl
+++ tools/kanopya_services.pl
@@ -2,7 +2,8 @@
 
 # Call all kanopya init script with given param
 
-my @services = qw(aggregator collector front executor mail-notifier openstack-sync rulesengine state-manager);
+my @services = qw(aggregator collector front executor mail-notifier openstack-sync rulesengine);
+# NOT the state-manager
 
 my $cmd = $ARGV[0] or die "Need param start/stop/restart/status";
 
