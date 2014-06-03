9b6a3ac79e83723996fe270052c31dc783267e29
Viktor Horvath <viktor.horvath@hederatech.com>
2014-06-03 14:06:24 +0200

    Mark migrations as applied for new installations: "--mark_as_applied_until"

--- scripts/database/mysql/sbin/update.pl
+++ scripts/database/mysql/sbin/update.pl
@@ -4,8 +4,25 @@
 use strict;
 use warnings;
 
-use DatabaseMigration::Transient;
 use Daemon;
+use DatabaseMigration::Transient;
+use Getopt::Long;
+use POSIX;
+
+my $mark_as_applied_until;
+GetOptions('mark_as_applied_until=s' => \$mark_as_applied_until);
+
+if ($mark_as_applied_until) {
+    if ($mark_as_applied_until eq 'now') {
+        $mark_as_applied_until = POSIX::strftime("%Y%m%d%H%M", localtime(time));
+    } elsif ($mark_as_applied_until !~ /^\d{12}$/) {
+        showHelp();
+        exit 1;
+    }
+    my $i = DatabaseMigration::Transient->markAsAppliedUntil($mark_as_applied_until);
+    print(($i == 1 ? "$i migration" : "$i migrations")." marked as applied.\n");
+    exit 0;
+}
 
 my @running_services = @{ Daemon->runningDaemons() };
 if (@running_services > 0) {
@@ -16,3 +33,16 @@ if (@running_services > 0) {
 }
 
 DatabaseMigration::Transient->runAll();
+
+
+
+sub showHelp() {
+    print <<"HELP";
+Usage: update.pl [ --mark_as_applied_until YearMonthDayHourMinute ]
+
+Without arguments, apply all pending migrations.
+
+With --mark_as_applied_until, does not execute any migration but
+mark all migrations earlier or equal to the given time as applied.    
+HELP
+}
